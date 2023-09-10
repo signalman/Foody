60c2f69515970abf09f72bab8ba0268b664ada7f
이규민 <lkm454545@naver.com>
2023-09-11 00:45:58 +0900

    chore: project init

---
+++ backend/Foody/src/test/java/com/foody/FoodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

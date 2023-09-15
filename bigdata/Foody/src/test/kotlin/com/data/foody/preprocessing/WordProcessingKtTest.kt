5fc14c6fd4632439a8e2b8c6b6e142a06c33b6ab
신상원 <dnjsml30@naver.com>
2023-09-15 16:26:27 +0900

    feat: 불용어 삭제
    Former-commit-id: bd7ee7cc81a2cd3779048db4fd03589b028b935e

--- bigdata/Foody/src/test/kotlin/com/data/foody/preprocessing/WordProcessingKtTest.kt
+++ bigdata/Foody/src/test/kotlin/com/data/foody/preprocessing/WordProcessingKtTest.kt
@@ -11,7 +11,7 @@ class WordProcessingKtTest {
     @DisplayName("불용어 읽어들이기 성공한다")
     fun t1() {
         val result = removeStopWords()
-        assertEquals(1620, result.size) // 예를 들어 10개의 불용어가 있다고 가정
-        assertEquals("조금", result[0]) // 첫 번째 불용어가 'the'라고 가정
+        assertEquals(1620, result.size)
+        assertTrue(result.contains("조금"))
     }
 }

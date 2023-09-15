6dae0871945ca322e677fd9b72fcf66b8056ee57
신상원 <dnjsml30@naver.com>
2023-09-15 16:47:12 +0900

    feat: 대체어로 변경

--- bigdata/Foody/src/main/kotlin/com/data/foody/preprocessing/WordProcessing.kt
+++ bigdata/Foody/src/main/kotlin/com/data/foody/preprocessing/WordProcessing.kt
@@ -3,22 +3,40 @@ package com.data.foody.preprocessing
 import org.springframework.core.io.ClassPathResource
 import java.io.BufferedReader
 import java.io.BufferedWriter
-import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 
 fun main() {
 
-    val list = removeStopWords()
-    readAndFilterRecipe(list)
+    //val list = removeStopWords()
+    // readAndFilterRecipe(list)
 
     val map = getReplaceWords()
-    replace(map)
+    readAndReplaceRecipe(map)
+
 }
 
-fun replace(map:HashMap<String,String>) {
+fun readAndReplaceRecipe(map: HashMap<String, String> ) {
+    val resource = ClassPathResource("data/preprocessing/filtered_final1_modified_stopwords.csv")
+    val inputStream = resource.inputStream
+    val reader = BufferedReader(InputStreamReader(inputStream, "EUC-KR"))
 
+    val writer = BufferedWriter(OutputStreamWriter(FileOutputStream("C:\\Users\\SSAFY\\Desktop\\특화 빅데이터\\S09P22C106\\bigdata\\Foody\\src\\main\\resources\\data\\preprocessing\\filtered_final1_modified_replace.csv"), "EUC-KR"))
+
+    reader.useLines { lines ->
+        lines.forEach { line ->
+            val cols = line.split(",")
+            val ingredients = cols[14].split(" ").map { map.getOrDefault(it, it) }
+            val replacedIngredients = ingredients.joinToString(" ")
+            val modifiedLine = cols.subList(0, 14).joinToString(",") + ",$replacedIngredients"
+
+            writer.write(modifiedLine)
+            writer.newLine()
+        }
+    }
+
+    writer.close()
 }
 
 fun removeStopWords(): Set<String> {
@@ -69,8 +87,11 @@ fun getReplaceWords(): HashMap<String, String> {
     reader.useLines { lines ->
         lines.forEach { line ->
             val cols = line.split(" ", limit = 2)
+            if(cols.size == 1) println(cols[0])
+            map[cols[0].trim()] = cols[1].trim()
         }
     }
 
     return map
 }
+

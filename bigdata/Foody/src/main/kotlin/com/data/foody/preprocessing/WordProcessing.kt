a39f72e0e490113f86572c16d6fe7d0b792b7c1f
신상원 <dnjsml30@naver.com>
2023-09-15 16:26:27 +0900

    feat: 불용어 삭제

--- bigdata/Foody/src/main/kotlin/com/data/foody/preprocessing/WordProcessing.kt
+++ bigdata/Foody/src/main/kotlin/com/data/foody/preprocessing/WordProcessing.kt
@@ -2,26 +2,51 @@ package com.data.foody.preprocessing
 
 import org.springframework.core.io.ClassPathResource
 import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.io.InputStreamReader
+import java.io.OutputStreamWriter
 
 fun main() {
 
     val list = removeStopWords()
-    print(list.size)
-    print(list[0])
+    readAndFilterRecipe(list)
 }
 
-fun removeStopWords(): MutableList<String> {
+fun removeStopWords(): Set<String> {
     val resource = ClassPathResource("data/preprocessing/stop_words.txt")
     val inputStream = resource.inputStream
     val reader = BufferedReader(InputStreamReader(inputStream, "UTF-8"))
 
-    val wordList: MutableList<String> = mutableListOf()
+    val stopWords = mutableSetOf<String>()
     reader.useLines { lines ->
         lines.forEach { line ->
-            wordList.add(line)
+            stopWords.add(line)
         }
     }
 
-    return wordList
+    return stopWords
+}
+
+fun readAndFilterRecipe(stopWords: Set<String>) {
+    val resource = ClassPathResource("data/preprocessing/filtered_final1.csv")
+    val inputStream = resource.inputStream
+    val reader = BufferedReader(InputStreamReader(inputStream, "EUC-KR"))
+
+    val writer = BufferedWriter(OutputStreamWriter(FileOutputStream("C:\\Users\\SSAFY\\Desktop\\특화 빅데이터\\S09P22C106\\bigdata\\Foody\\src\\main\\resources\\data\\preprocessing\\filtered_final1_modified_stopwords.csv"), "EUC-KR"))
+
+    reader.useLines { lines ->
+        lines.forEach { line ->
+            val cols = line.split(",")
+            val ingredients = cols[14].split(" ").filter { it !in stopWords }
+            val filteredIngredients = ingredients.joinToString(" ")
+            val modifiedLine = cols.subList(0, 14).joinToString(",") + ",$filteredIngredients"
+
+            writer.write(modifiedLine)
+            writer.newLine()
+        }
+    }
+
+    writer.close()
 }

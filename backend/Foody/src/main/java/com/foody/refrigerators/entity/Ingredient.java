69ed9d296635a7d43a637b6e37a9b95653962ad4
조희라 <bunny7531@naver.com>
2023-09-18 16:02:49 +0900

    refactor: annotation 설정 추가

--- backend/Foody/src/main/java/com/foody/refrigerators/entity/Ingredient.java
+++ backend/Foody/src/main/java/com/foody/refrigerators/entity/Ingredient.java
@@ -1,25 +1,23 @@
 package com.foody.refrigerators.entity;
 
 import com.foody.global.entity.BaseEntity;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Ingredient extends BaseEntity {
     @Column(nullable = false)
     String ingredientName;
     @Column
     String iconImg;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(nullable = false)
     IngredientCategory ingredientCategory;
 }

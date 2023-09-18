bdf8c81986abd0ecc4a4fc69ce07a7940694f7fa
조희라 <bunny7531@naver.com>
2023-09-18 17:44:04 +0900

    feat: 냉장고 선택 재료 등록 API 설계

--- backend/Foody/src/main/java/com/foody/refrigerators/service/RefrigeratorsService.java
+++ backend/Foody/src/main/java/com/foody/refrigerators/service/RefrigeratorsService.java
@@ -1,14 +1,20 @@
 package com.foody.refrigerators.service;
 
 import com.foody.global.exception.ErrorCode;
+import com.foody.refrigerators.dto.request.InsertIngredientRequest;
 import com.foody.refrigerators.dto.response.SearchIngredientResponse;
 import com.foody.refrigerators.entity.Ingredient;
+import com.foody.refrigerators.entity.RefrigeratorIngredient;
 import com.foody.refrigerators.exception.IngredientException;
 import com.foody.refrigerators.repository.IngredientRepository;
+import com.foody.refrigerators.repository.RefrigeratorIngredientRepository;
+import com.foody.user.entity.User;
+import com.foody.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -17,6 +23,8 @@ import java.util.List;
 public class RefrigeratorsService {
 
     private final IngredientRepository ingredientRepository;
+    private final RefrigeratorIngredientRepository refrigeratorIngredientRepository;
+    private final UserService userService;
 
     public Ingredient findByIngredientId(Long ingredientId) {
         return ingredientRepository.findById(ingredientId)
@@ -31,6 +39,23 @@ public class RefrigeratorsService {
         log.debug("first ingredient name : " + searchResult.get(0).getIngredientName());
         return searchResult;
     }
+
+    public void insertIngredient(String email, InsertIngredientRequest insertIngredientRequest) {
+
+        List<RefrigeratorIngredient> ingredients = new ArrayList<>();
+
+        for(long ingredientId : insertIngredientRequest.IngredientId()) {
+            User user = userService.findByEmail(email);
+            Ingredient ingredient = findByIngredientId(ingredientId);
+
+            RefrigeratorIngredient refrigeratorIngredient =
+                    RefrigeratorIngredient.from(user, ingredient);
+            ingredients.add(refrigeratorIngredient);
+        }
+
+        refrigeratorIngredientRepository.saveAll(ingredients);
+    }
+
 
 
 }

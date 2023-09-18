3c73c55bdb307ee77f55f1a8375989298c06bb44
Kim SeoungHyeon <kim0221_@naver.com>
2023-09-18 17:43:20 +0900

    feat : ToggleSwitch 컴퍼넌트 생성

--- frontend/foody/src/components/template/DevTemplate/DevTemplate.tsx
+++ frontend/foody/src/components/template/DevTemplate/DevTemplate.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './DevTemplate.scss';
 import LargeButton from 'components/atom/LargeButton/LargeButton';
+import ToggleSwitch from 'components/atom/ToggleSwitch/ToggleSwitch';
 import LargeButtonColor from 'constants/color';
 
 function DevTemplate() {
@@ -8,6 +9,7 @@ function DevTemplate() {
 		<div>
 			<LargeButton value="hi" buttonColor={LargeButtonColor.Green} />
 			<LargeButton value="hi" buttonColor={LargeButtonColor.Gray} />
+			<ToggleSwitch isRefri onText="냉장고" offText="선반" />
 		</div>
 	);
 }

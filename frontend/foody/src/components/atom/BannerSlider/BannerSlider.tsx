3598df43f6d272f16bbdd6c99d1d7993bff5ee8f
임희선 <gmltjs4107@gmail.com>
2023-09-19 17:55:52 +0900

    feat: 홈 화면 컴포넌트로 변경

---
+++ frontend/foody/src/components/atom/BannerSlider/BannerSlider.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'swiper/css';
+import './BannerSlider.scss';
+
+function BannerSlider() {
+	return <div className="banner-slider-container">배너 슬라이더</div>;
+}
+
+export default BannerSlider;

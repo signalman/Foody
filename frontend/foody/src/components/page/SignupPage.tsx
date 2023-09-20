94265011d5dda5cdaa2df84096e3f420dd019ddc
Kim SeoungHyeon <kim0221_@naver.com>
2023-09-20 15:04:32 +0900

    feat:Signup페이지 step3 컴퍼넌트 생성

--- frontend/foody/src/components/page/SignupPage.tsx
+++ frontend/foody/src/components/page/SignupPage.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import ProgressBar from 'components/molecule/ProgressBar/ProgressBar';
+import SignupStep1 from 'components/organism/signup/SignupStep1/SignupStep1';
+import React, { useState } from 'react';
 
 function SignupPage() {
-	return <div />;
+	const [value, setValue] = useState('');
+	const check = [true, true, false, false, false];
+
+	return (
+		<div>
+			<ProgressBar ProgressCheck={check} />
+			<SignupStep1 setValue={setValue} value={value} />
+		</div>
+	);
 }
 
 export default SignupPage;

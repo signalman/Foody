91160b7c2e0ce340789331f171f4143fc0dcf581
임희선 <gmltjs4107@gmail.com>
2023-09-20 17:54:30 +0900

    feat: custom alert 수정 중

---
+++ frontend/foody/src/components/molecule/CustomBottomSheet/CustomBottomSheet.tsx
@@ -0,0 +1,26 @@
+import React, { Dispatch, ReactNode } from 'react';
+import { BottomSheet } from 'react-spring-bottom-sheet';
+import 'react-spring-bottom-sheet/dist/style.css';
+import SheetContent from 'components/atom/SheetContent/SheetContent';
+import './CustomBottomSheet.scss';
+
+interface CustomBottomSheetProps {
+	children: ReactNode;
+	open: boolean;
+	setOpen: Dispatch<React.SetStateAction<boolean>>;
+}
+
+function CustomBottomSheet({ children, open, setOpen }: CustomBottomSheetProps) {
+	return (
+		<div className="custom-bottom-sheet-container">
+			<BottomSheet open={open} onDismiss={() => setOpen(false)}>
+				<SheetContent>
+					<div className="wrap">{children}</div>
+					<div style={{ height: '60vh' }} />
+				</SheetContent>
+			</BottomSheet>
+		</div>
+	);
+}
+
+export default CustomBottomSheet;

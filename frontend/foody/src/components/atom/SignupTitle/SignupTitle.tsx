52b65f1fa1232e3b7552fec902127489e7d98352
Kim SeoungHyeon <kim0221_@naver.com>
2023-09-20 15:03:23 +0900

    hotfix:SignupTitle scss 수정

--- frontend/foody/src/components/atom/SignupTitle/SignupTitle.tsx
+++ frontend/foody/src/components/atom/SignupTitle/SignupTitle.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './SignupTitle.scss';
 
 interface SignupTitleProps {
 	value: string;
 }
 
 function SignupTitle({ value }: SignupTitleProps) {
-	return <span className="title">{value}</span>;
+	return <h2 className="title">{value}</h2>;
 }
 
 export default SignupTitle;

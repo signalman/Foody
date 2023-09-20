ffc1e20564b785e9230a78bb35c97efe6649ede8
임희선 <gmltjs4107@gmail.com>
2023-09-20 09:35:57 +0900

    feat: ContentsLayout 추가

---
+++ frontend/foody/src/constants/Margin.ts
@@ -0,0 +1,15 @@
+enum LayoutBottomMargin {
+	mb30 = 'mb-30',
+	mb20 = 'mb-20',
+	mb10 = 'mb-10',
+	mb0 = 'mb-0',
+}
+
+export enum LayoutTopMargin {
+	mt30 = 'mt-30',
+	mt20 = 'mt-20',
+	mt10 = 'mt-10',
+	mt0 = 'mt-0',
+}
+
+export default LayoutBottomMargin;

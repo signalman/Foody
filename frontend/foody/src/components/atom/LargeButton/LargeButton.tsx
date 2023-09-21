f26cf68f6c451b54bd468664e78e48df9c0b40b5
Kim SeoungHyeon <kim0221_@naver.com>
2023-09-21 14:20:24 +0900

    feat: Signup 닉네임입력 컴퍼넌트 생성

--- frontend/foody/src/components/atom/LargeButton/LargeButton.tsx
+++ frontend/foody/src/components/atom/LargeButton/LargeButton.tsx
@@ -6,15 +6,16 @@ import LargeButtonColor from 'constants/color';
 interface BottomBtnProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	value?: string;
 	buttonColor?: LargeButtonColor;
+	buttonClick: () => void;
 }
 
 interface ImgProps {
 	imgsrc: string;
 }
 
-function LargeButton({ value, buttonColor, imgsrc }: BottomBtnProps & ImgProps) {
+function LargeButton({ value, buttonColor, buttonClick, imgsrc }: BottomBtnProps & ImgProps) {
 	return (
-		<button type="button" className={classNames('button-value', buttonColor)}>
+		<button type="button" onClick={buttonClick} className={classNames('button-value', buttonColor)}>
 			{imgsrc && imgsrc.length > 0 && <img className="img-size" src={imgsrc} alt="" />}
 			{value}
 		</button>
--- frontend/foody/src/components/organism/signup/SignupStep1/SignupStep1.tsx
+++ frontend/foody/src/components/organism/signup/SignupStep1/SignupStep1.tsx
@@ -5,26 +5,32 @@ import LargeButtonColor from 'constants/color';
 import React, { useEffect, useState } from 'react';
 
 interface SignupStep1Props {
-	value: string;
-	setValue: (value: string) => void;
+	nickname: string;
+	setNickname: (nickname: string) => void;
+	nextButton: () => void;
 }
 
-function SignupStep1({ value, setValue }: SignupStep1Props) {
+function SignupStep1({ nickname, setNickname, nextButton }: SignupStep1Props) {
 	const [check, setCheck] = useState(false);
 
 	useEffect(() => {
-		if (value.length > 0) {
+		if (nickname.length > 0) {
 			setCheck(true);
 		} else {
 			setCheck(false);
 		}
-	}, [value]);
+	}, [nickname]);
 
 	return (
 		<div className="step1-container">
 			<SignupTitle value="닉네임을 알려주세요" />
-			<UnderlineInput onChangeValue={setValue} placeholder="닉네임" unit="" value={value} />
-			<LargeButton imgsrc="" value="확인" buttonColor={check ? LargeButtonColor.Green : LargeButtonColor.Gray} />
+			<UnderlineInput onChangeValue={setNickname} placeholder="닉네임" unit="" value={nickname} />
+			<LargeButton
+				buttonClick={nextButton}
+				imgsrc=""
+				value="확인"
+				buttonColor={check ? LargeButtonColor.Green : LargeButtonColor.Gray}
+			/>
 		</div>
 	);
 }

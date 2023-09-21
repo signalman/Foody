9529a9c8d16ca94c4422ea55ad3852e571c513aa
Kim SeoungHyeon <kim0221_@naver.com>
2023-09-21 14:21:43 +0900

    feat:Signup페이지 기능추가

--- frontend/foody/src/components/template/SignupTemplate/SignupTemplate.tsx
+++ frontend/foody/src/components/template/SignupTemplate/SignupTemplate.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import './Signup.scss';
+import React, { ReactNode } from 'react';
+import './SignupTemplate.scss';
+import Header from 'components/organism/Header/Header';
+import LayoutPadding from 'constants/Padding';
+import LayoutBottomMargin, { LayoutTopMargin } from 'constants/Margin';
+import Layout from '../Layout/Layout';
+import ContentsLayout from '../ContentsLayout/ContentsLayout';
 
-function Signup() {
-	return <div />;
+function SignupTemplate({ children }: { children: ReactNode[] }) {
+	return (
+		<>
+			<Header />
+			<Layout marginTop={LayoutTopMargin.mt10} marginBottom={LayoutBottomMargin.mbTabbar} padding={LayoutPadding.p20}>
+				<ContentsLayout>{children[0]}</ContentsLayout>
+				<ContentsLayout>{children[1]}</ContentsLayout>
+				<ContentsLayout>{children[2]}</ContentsLayout>
+				<ContentsLayout>{children[3]}</ContentsLayout>
+				<ContentsLayout>{children[4]}</ContentsLayout>
+				<ContentsLayout>{children[5]}</ContentsLayout>
+			</Layout>
+		</>
+	);
 }
 
-export default Signup;
+export default SignupTemplate;

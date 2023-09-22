4cfd6f0517a49606119d11d6e5231c16b2a4a7f0
Kim SeoungHyeon <kim0221_@naver.com>
2023-09-22 15:04:47 +0900

    feat:Google로그인

--- frontend/foody/src/components/page/LoginPage.tsx
+++ frontend/foody/src/components/page/LoginPage.tsx
@@ -1,16 +1,53 @@
 import LoginTemplate from 'components/template/LoginTemplate/LoginTemplate';
-import React, { memo } from 'react';
+import React, { memo, useCallback, useEffect } from 'react';
 import SocialLoginButton from 'components/atom/SocialLoginButton/SocialLoginButton';
 import login from 'utils/api/auth';
+import LocalStorage from 'constants/LocalStorage';
+import { setCookie } from 'constants/Cookie';
+import useMovePage from 'hooks/useMovePage';
 import loginLogo from '../../assets/icons/mainLogo.png';
 import googleLogo from '../../assets/icons/googleLogo.png';
 
 const LoginPage = memo(() => {
+	const { movePage } = useMovePage();
+
+	const fetchRedirectLocation = useCallback(() => {
+		fetch('/login/oauth2/code/google', {
+			redirect: 'manual',
+		}).then((response) => {
+			if (response.status === 200) {
+				const urlParams = new URLSearchParams(window.location.search);
+				const accessToken = urlParams.get('accessToken');
+				const refreshToken = urlParams.get('refreshToken');
+				const user = urlParams.get('user');
+
+				if (!accessToken || !refreshToken || !user) {
+					throw new Error('No saved Token');
+				}
+
+				LocalStorage.setItem('accesstoken', accessToken);
+				setCookie('refreshtoken', refreshToken);
+
+				if (user === '1') {
+					movePage('/home', null);
+				} else if (user === '0') {
+					movePage('/signup', null);
+				}
+			}
+		});
+	}, [movePage]);
+
 	const test2 = () => {
-		// const redirectUrl = 'http://localhost/oauth2/authorization/google';
-		// window.location.href = redirectUrl;
 		login();
 	};
+
+	useEffect(() => {
+		const test = window.location.search;
+
+		if (test !== '') {
+			fetchRedirectLocation();
+		}
+	}, [fetchRedirectLocation]);
 
 	return (
 		<LoginTemplate>

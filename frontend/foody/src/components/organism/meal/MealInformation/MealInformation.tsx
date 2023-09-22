eee92257df0f7bef066799c9bd191a4a94780ef7
Kim SeoungHyeon <kim0221_@naver.com>
2023-09-22 17:30:04 +0900

    feat:식단 레이아웃

---
+++ frontend/foody/src/components/organism/meal/MealInformation/MealInformation.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MealInformation.scss';
+
+function MealInformation() {
+	return (
+		<div className="meal-information">
+			<p>information</p>
+		</div>
+	);
+}
+
+export default MealInformation;
---
+++ frontend/foody/src/components/organism/meal/NutrientOfDay/NutrientOfDay.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './NutrientOfDay.scss';
+
+function NutrientOfDay() {
+	return (
+		<div>
+			<div className="nutrient-of-day-title">
+				<p>일일영양소</p>
+				<p>더보기</p>
+			</div>
+			<div className="box">
+				<p>test</p>
+			</div>
+		</div>
+	);
+}
+
+export default NutrientOfDay;

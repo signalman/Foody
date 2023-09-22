73dd1da17cf84da1ff616c6cee5e8f9e7e7841c7
Kim SeoungHyeon <kim0221_@naver.com>
2023-09-22 17:30:04 +0900

    feat:식단 레이아웃

---
+++ frontend/foody/src/components/organism/meal/MealCalendar/MealCalendar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MealCalendar.scss';
+
+function MealCalendar() {
+	return (
+		<div className="calendar-box">
+			<p>Calendar</p>
+		</div>
+	);
+}
+
+export default MealCalendar;

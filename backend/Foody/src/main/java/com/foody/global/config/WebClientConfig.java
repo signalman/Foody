108331dbcf40675a1a56da63b58ace6966331b35
조희라 <bunny7531@naver.com>
2023-09-22 17:51:08 +0900

    feat: webClient 설정 추가

---
+++ backend/Foody/src/main/java/com/foody/global/config/WebClientConfig.java
@@ -0,0 +1,25 @@
+package com.foody.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${NAVER_CLIENT_ID}")
+    private String clientId;
+
+    @Value("${NAVER_CLIENT_SECRET}")
+    private String clientSecret;
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("https://openapi.naver.com/v1/search/shop.json?query=")
+                .defaultHeader("X-Naver-Client-Id", clientId)
+                .defaultHeader("X-Naver-Client-Secret", clientSecret)
+                .build();
+    }
+}

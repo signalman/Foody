ee2aa3961966696d64f97b6b45735314c5f3b87a
임희선 <gmltjs4107@gmail.com>
2023-09-24 01:55:16 +0900

    test: 카메라 테스트

--- frontend/foody/src/components/organism/IngredientRegistOCR/IngredientRegistOCR.tsx
+++ frontend/foody/src/components/organism/IngredientRegistOCR/IngredientRegistOCR.tsx
@@ -3,7 +3,7 @@ import './IngredientRegistOCR.scss';
 import Layout from 'components/template/Layout/Layout';
 import { useRecoilState } from 'recoil';
 import tabbarState from 'recoil/atoms/tabbarState';
-import { BiCamera } from 'react-icons/bi';
+import OCRCamera from 'components/atom/OCRCamera/OCRCamera';
 import SubHeader from '../SubHeader/SubHeader';
 
 function IngredientRegistOCR({ setOpen }: { setOpen: Dispatch<React.SetStateAction<boolean>> }) {
@@ -17,17 +17,9 @@ function IngredientRegistOCR({ setOpen }: { setOpen: Dispatch<React.SetStateActi
 	return (
 		<div className="ingredient-regist-OCR-container">
 			<SubHeader isBack title="재료 등록" handleMove={handleMove} />
-			<Layout>비디오</Layout>
-			<div className="btn-container">
-				<button
-					type="button"
-					onClick={() => {
-						console.log('카메라');
-					}}
-				>
-					<BiCamera size={25} />
-				</button>
-			</div>
+			<Layout>
+				<OCRCamera />
+			</Layout>
 		</div>
 	);
 }

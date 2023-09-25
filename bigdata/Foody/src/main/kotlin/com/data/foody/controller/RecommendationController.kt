f6ecdcf85f2bbab5b51a7bab2b68a5c007f3d799
신상원 <dnjsml30@naver.com>
2023-09-25 23:07:23 +0900

    feat: 냉장고 기반 유사한 레시피 추출 기능 구현

---
+++ bigdata/Foody/src/main/kotlin/com/data/foody/controller/RecommendationController.kt
@@ -0,0 +1,19 @@
+package com.data.foody.controller
+
+import com.data.foody.recommendation.RecommendationService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/data")
+@RestController
+class RecommendationController(private val recommendationService: RecommendationService) {
+
+    @PostMapping("/ingredient")
+    suspend fun getRecommendationByIngredient(@RequestBody ingredients: String): ResponseEntity<String> {
+
+        return ResponseEntity.ok().body(recommendationService.getRecommendationByIngredients(ingredients))
+    }
+}
---
+++ bigdata/Foody/src/main/kotlin/com/data/foody/recommendation/RecommendationService.kt
@@ -0,0 +1,26 @@
+package com.data.foody.recommendation
+
+import com.data.foody.algorithm.Similarity
+import org.springframework.stereotype.Service
+
+@Service
+class RecommendationService(private val dataService: DataService) {
+
+    private val similarity = Similarity()
+
+    suspend fun getRecommendationByIngredients(ingredients: String): String {
+        val allRecipes = dataService.findAllValuesWithCoroutine()
+
+        // 각 레시피에 대해 유사도 계산
+        val similarityScores = allRecipes?.map { recipe ->
+            val score = similarity.cosineSimilarity(ingredients, recipe.ingredients_concat)
+            Pair(recipe, score)
+        }
+
+        // 유사도에 따라 레시피 정렬 및 상위 5개 추출 후, 해당 레시피들의 recipe_id만 추출
+        val topRecipeIds = similarityScores?.sortedByDescending { it.second }?.take(5)?.map { it.first.recipe_id }
+
+        // Recipe IDs를 하나의 문자열로 반환
+        return topRecipeIds?.joinToString(", ") ?: ""
+    }
+}

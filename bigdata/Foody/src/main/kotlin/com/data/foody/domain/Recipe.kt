ca83729bae3e68009788c73055ae3a70d50befec
신상원 <dnjsml30@naver.com>
2023-09-25 11:27:00 +0900

    feat: recipe data class 생성

---
+++ bigdata/Foody/src/main/kotlin/com/data/foody/domain/Recipe.kt
@@ -0,0 +1,25 @@
+package com.data.foody.domain
+
+data class Recipe(
+        val recipe_id: Long,
+        val name: String,
+        val ingredients: String,
+        val ingredients_concat: String,
+        val ingredient_count: Int,
+        val energy: Float,
+        val carbohydrates: Float,
+        val protein: Float,
+        val fats: Float,
+        val dietaryFiber: Float,
+        val calcium: Float,
+        val sodium: Float,
+        val iron: Float,
+        val vitaminA: Float,
+        val vitaminC: Float,
+        val difficulty: String,
+        val servers: Int,
+        val food_preparation_methods: String,
+        val food_situations: String,
+        val food_ingredients: String,
+        val food_types: String
+)

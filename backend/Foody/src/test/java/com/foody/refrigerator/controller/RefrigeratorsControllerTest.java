b5107b49c1b63be60039dfbe7acb57099b76a9c8
jjoyra <bunny7531@naver.com>
2023-09-26 08:43:58 +0900

    test: 재료 등록 테스트 코드 작성

--- backend/Foody/src/test/java/com/foody/refrigerator/controller/RefrigeratorsControllerTest.java
+++ backend/Foody/src/test/java/com/foody/refrigerator/controller/RefrigeratorsControllerTest.java
@@ -1,11 +1,18 @@
 package com.foody.refrigerator.controller;
 
+import com.foody.refrigerators.dto.request.CustomIngredientRequest;
+import com.foody.refrigerators.dto.request.InsertIngredientRequest;
 import com.foody.refrigerators.dto.response.SearchIngredientResponse;
+import com.foody.refrigerators.dto.response.UserRefrigeratorResponse;
+import com.foody.security.util.LoginInfo;
 import com.foody.util.ControllerTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -13,8 +20,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
@@ -65,11 +73,72 @@ public class RefrigeratorsControllerTest extends ControllerTest {
                 );
     }
 
-//    @Test
-//    @DisplayName("냉장고에_재료를_등록한다.")
-//    public void registerIngredient() throws Exception {
-//        mockMvc.perform(post(baseUrl))
-//    }
+    @Test
+    @DisplayName("냉장고에_재료를_등록한다.")
+    public void registerIngredient() throws Exception {
+        LoginInfo loginInfo = new LoginInfo("lkm454545@gmail.com");
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(new TestingAuthenticationToken(loginInfo, null));
+        SecurityContextHolder.setContext(securityContext);
+
+        List<Long> ingredientIds = new ArrayList<>();
+        ingredientIds.add(1L);
+        List<CustomIngredientRequest> customIngredientRequests = new ArrayList<>();
+        customIngredientRequests.add(new CustomIngredientRequest(1L, "토마토마"));
+        InsertIngredientRequest insertIngredientRequest = new InsertIngredientRequest(ingredientIds, customIngredientRequests);
+
+//        doNothing().when(refrigeratorsService.insertIngredient())
+
+        mockMvc.perform(post(baseUrl)
+                .with(authentication(new TestingAuthenticationToken(loginInfo, null)))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(insertIngredientRequest))
+        ).andExpect(status().isNoContent())
+                .andDo(
+                        document("refrigerators/registerIngredient",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                requestFields(
+                                        fieldWithPath("ingredients").description("ingredient PK"),
+                                        fieldWithPath("customIngredients").description("사용자 입력 재료"),
+                                        fieldWithPath("customIngredients[].ingredientCategoryId").description("재료 카테고리 PK"),
+                                        fieldWithPath("customIngredients[].ingredientName").description("재료 이름")
+                                ))
+                );
+    }
+
+    @Test
+    @DisplayName("냉장고의_재료를_조회한다.")
+    public void getUserRefrigerator() throws Exception {
+
+        List<UserRefrigeratorResponse> list = new ArrayList<>();
+
+        list.add(new UserRefrigeratorResponse(1L,"마늘",1L, "2023.09.20"));
+        list.add(new UserRefrigeratorResponse(6L,"버터",1L, "2023.09.20"));
+
+        LoginInfo loginInfo = new LoginInfo("lkm454545@gmail.com");
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(new TestingAuthenticationToken(loginInfo, null));
+        SecurityContextHolder.setContext(securityContext);
+
+        when(refrigeratorsService.getUserRefrigerator(loginInfo.email())).thenReturn(list);
+
+        mockMvc.perform(get(baseUrl)
+                .with(authentication(new TestingAuthenticationToken(loginInfo, null)))
+                ).andExpect(status().isOk())
+                .andDo(
+                        document("refrigerators/getUserRefrigerator",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                responseFields(
+                                        fieldWithPath("[]").description("냉장고 재료 목록"),
+                                        fieldWithPath("[].ingredientId").type(JsonFieldType.NUMBER).description("ingredient PK"),
+                                        fieldWithPath("[].ingredientName").type(JsonFieldType.STRING).description("재료 이름"),
+                                        fieldWithPath("[].ingredientCategoryId").type(JsonFieldType.NUMBER).description("ingredientCategory Id"),
+                                        fieldWithPath("[].createDate").type(JsonFieldType.STRING).description("생성 날짜")
+                                ))
+                );
+    }
 
 
 }

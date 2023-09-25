c3bb435b3bdb02768f20424fc653b85aeff48297
신상원 <dnjsml30@naver.com>
2023-09-25 22:16:11 +0900

    test: redis keys 호출 시간 테스트
    Former-commit-id: ffc351e0328e791d463a5640e10e5564c0ed1c47

--- bigdata/Foody/src/test/kotlin/com/data/foody/recommendation/RecommendationServiceTest.kt
+++ bigdata/Foody/src/test/kotlin/com/data/foody/recommendation/RecommendationServiceTest.kt
@@ -1,12 +1,14 @@
 package com.data.foody.recommendation
 
 import com.data.foody.domain.Recipe
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.test.context.ActiveProfiles
+import kotlin.system.measureTimeMillis
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -22,5 +24,11 @@ class RecommendationServiceTest {
     @DisplayName("redis의 모든 keys 불러와진다")
     fun shouldFetchAllKeysFromRedis() {
 
+        val elapsedTime = measureTimeMillis {
+        val keys: Set<String> = redisTemplate.keys("*")
+            assertEquals(keys.size, 120738)
+        }
+
+        println("Elapsed time: $elapsedTime ms")
     }
 }

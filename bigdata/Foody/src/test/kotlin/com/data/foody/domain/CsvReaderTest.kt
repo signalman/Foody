6c80f330b09db6e3b8e16d8a13adac73b67d1181
신상원 <dnjsml30@naver.com>
2023-09-25 11:59:51 +0900

    test: csv파일 읽기 테스트

---
+++ bigdata/Foody/src/test/kotlin/com/data/foody/domain/CsvReaderTest.kt
@@ -0,0 +1,34 @@
+package com.data.foody.domain
+
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.core.io.ClassPathResource
+import kotlin.system.measureTimeMillis
+
+class CsvReaderTest {
+
+    @Test
+    @DisplayName("CSV 파일 읽기 테스트")
+    fun read_csv() {
+
+        val resource = ClassPathResource("data/preprocessing/recipe_information.csv")
+        val recipeIndex: List<String> = listOf(
+                "recipe_id", "ingredients", "ingredients_concat", "ingredient_count",
+                "energy", "carbohydrates", "protein", "fats", "dietaryFiber", "calcium",
+                "sodium", "iron", "vitaminA", "vitaminC", "difficulty", "servers",
+                "food_preparation_methods", "food_situations", "food_ingredients", "food_types"
+        )
+
+        var target: List<String> = listOf()
+        val elapsedTime = measureTimeMillis {
+            val rows: List<List<String>> = csvReader().readAll(resource.inputStream)
+            target = rows[0]
+        }
+
+        println("Elapsed time: $elapsedTime ms")
+        assertEquals(target, recipeIndex)
+
+    }
+}

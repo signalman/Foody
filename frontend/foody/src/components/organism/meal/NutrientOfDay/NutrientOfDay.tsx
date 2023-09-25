fd5a9a7b02fb13ad1b5d4d79eef02ca13efe9414
Kim SeoungHyeon <kim0221_@naver.com>
2023-09-25 09:33:18 +0900

    feat:영양소 정보 테이블 컴포넌트 생성

--- frontend/foody/src/components/organism/meal/NutrientOfDay/NutrientOfDay.tsx
+++ frontend/foody/src/components/organism/meal/NutrientOfDay/NutrientOfDay.tsx
@@ -1,16 +1,49 @@
 import React from 'react';
 import './NutrientOfDay.scss';
+import BarChart from 'components/atom/BarChart/BarChart';
+import { BarColor } from 'constants/color';
+import NutrientInfo from 'components/atom/NutrientInfo/NutrientInfo';
+import Layout from 'components/template/Layout/Layout';
+import LayoutBottomMargin, { LayoutTopMargin } from 'constants/Margin';
+import LayoutPadding from 'constants/Padding';
 
 function NutrientOfDay() {
+	const data = {
+		tan: 196,
+		dan: 44,
+		ji: 46,
+
+		totalTan: 257,
+		totalDan: 93.5,
+		totalJi: 70,
+	};
 	return (
-		<div>
-			<div className="nutrient-of-day-title">
-				<p>일일영양소</p>
-				<p>더보기</p>
-			</div>
-			<div className="box">
-				<p>test</p>
-			</div>
+		<div className="nutrient-main-box">
+			<Layout marginTop={LayoutTopMargin.mt10} marginBottom={LayoutBottomMargin.mb10} padding={LayoutPadding.p10}>
+				<div className="nutrient-sub-box">
+					<div className="nutrient-little-box">
+						<p>탄수화물</p>
+						<div className="nutrient">
+							<BarChart barcolor={BarColor.Null} total={data.totalTan} value={data.tan} />
+							<NutrientInfo total={data.tan} value={data.totalTan} />
+						</div>
+					</div>
+					<div className="nutrient-little-box">
+						<p>단백질</p>
+						<div className="nutrient">
+							<BarChart barcolor={BarColor.Null} total={data.totalDan} value={data.dan} />
+							<NutrientInfo total={data.dan} value={data.totalDan} />
+						</div>
+					</div>
+					<div className="nutrient-little-box">
+						<p>지방</p>
+						<div className="nutrient">
+							<BarChart barcolor={BarColor.Null} total={data.totalJi} value={data.ji} />
+							<NutrientInfo total={data.ji} value={data.totalJi} />
+						</div>
+					</div>
+				</div>
+			</Layout>
 		</div>
 	);
 }

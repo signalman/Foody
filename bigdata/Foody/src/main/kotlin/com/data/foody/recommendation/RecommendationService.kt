5a20b333bc33b4752d1b6bce6b626dbc2ce09cb6
신상원 <dnjsml30@naver.com>
2023-09-25 23:53:59 +0900

    refactor: 연산 횟수 계산 로직 추가
    Former-commit-id: ff93d4afd8ba78b9d5227b2cda513334b6a4491f

--- bigdata/Foody/src/main/kotlin/com/data/foody/recommendation/RecommendationService.kt
+++ bigdata/Foody/src/main/kotlin/com/data/foody/recommendation/RecommendationService.kt
@@ -1,6 +1,8 @@
 package com.data.foody.recommendation
 
 import com.data.foody.algorithm.Similarity
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import org.springframework.stereotype.Service
 
 @Service
@@ -17,13 +19,37 @@ class RecommendationService(private val dataService: DataService) {
         // 각 레시피에 대해 유사도 계산
         val similarityScores = filteredRecipes?.map { recipe ->
             val score = similarity.cosineSimilarity(ingredients, recipe.ingredients_concat)
-            Pair(recipe, score)
+            Pair(recipe.recipe_id, score)
         }
 
-        // 유사도에 따라 레시피 정렬 및 상위 5개 추출 후, 해당 레시피들의 recipe_id만 추출
-        val topRecipeIds = similarityScores?.sortedByDescending { it.second }?.take(5)?.map { it.first.recipe_id }
+        // 유사도에 따라 레시피 정렬 및 상위 5개 추출
+        val topRecipesWithScores = similarityScores?.sortedByDescending { it.second }?.take(5)
 
-        // Recipe IDs를 하나의 문자열로 반환
-        return topRecipeIds?.joinToString(", ") ?: ""
+        // Recipe IDs와 유사도를 하나의 문자열로 반환
+        return topRecipesWithScores?.joinToString(", ") { "${it.first}: ${it.second}" } ?: ""
+    }
+
+    suspend fun getRecommendationByIngredientsWithCoroutine(ingredients: String): String {
+        val allRecipes = dataService.findAllValuesWithCoroutine()
+
+        // 재료의 수가 5개 이상인 레시피만 필터링
+        val filteredRecipes = allRecipes?.filter { it.ingredient_count >= 5 }
+
+        // 각 레시피에 대해 유사도 계산 (병렬 처리)
+        val similarityScores = coroutineScope {
+            filteredRecipes?.map { recipe ->
+                async {
+                    val score = similarity.cosineSimilarity(ingredients, recipe.ingredients_concat)
+                    Pair(recipe.recipe_id, score)
+                }
+            }?.map { it.await() }
+        }
+
+        // 유사도에 따라 레시피 정렬 및 상위 5개 추출
+        val topRecipesWithScores = similarityScores?.sortedByDescending { it.second }?.take(5)
+
+        println("########################## ${similarity.operationCount} #############################")
+        // Recipe IDs와 유사도를 하나의 문자열로 반환
+        return topRecipesWithScores?.joinToString(", ") { "${it.first}: ${it.second}" } ?: ""
     }
 }

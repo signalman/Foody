3a92be000407bfbc8ed8973c662b9fa493c74bd2
신상원 <dnjsml30@naver.com>
2023-09-26 21:19:28 +0900

    feat: commandLineRunner 구현

--- backend/Foody/src/main/java/com/foody/global/util/DataLoader.java
+++ backend/Foody/src/main/java/com/foody/global/util/DataLoader.java
@@ -1,20 +1,25 @@
 package com.foody.global.util;
 
 import com.foody.recipe.entity.Recipe;
+import com.foody.recipe.repository.RecipeCustomRepository;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class CsvUtil {
+public class DataLoader {
 
-    public List<Recipe> readRecipesFromCSV(String filePath) throws IOException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
+    public List<Recipe> readRecipesFromCSV(Path filePath) throws IOException {
+        try (Reader reader = Files.newBufferedReader(filePath)) {
             ColumnPositionMappingStrategy<Recipe> strategy = new ColumnPositionMappingStrategy<>();
             strategy.setType(Recipe.class);
             String[] memberFieldsToBindTo = {
@@ -31,5 +36,18 @@ public class CsvUtil {
 
             return csvToBean.parse();
         }
+    }
+
+    @Bean
+    public CommandLineRunner dataLoad(RecipeCustomRepository recipeCustomRepository) {
+        return (args) -> {
+            boolean exists = recipeCustomRepository.isExistsData();
+            ClassPathResource resource = new ClassPathResource("recipe/foody_recipe.csv");
+            Path path = resource.getFile().toPath();
+            if(!exists) {
+                List<Recipe> recipes = readRecipesFromCSV(path);
+                recipeCustomRepository.bulkInsert(recipes);
+            }
+        };
     }
 }

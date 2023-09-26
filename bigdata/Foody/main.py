797206122ebf9219f6ff80e3219e820958d836ee
신상원 <dnjsml30@naver.com>
2023-09-26 09:12:16 +0900

    feat: 코사인 유사도
    Former-commit-id: 6dce7a2f108943a60c316982253b708b9890e094

--- bigdata/Foody/main.py
+++ bigdata/Foody/main.py
@@ -6,8 +6,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}

dbf16f245440775c0e3ed4ddada5044a6ffd6ab5
신상원 <dnjsml30@naver.com>
2023-09-26 20:51:15 +0900

    feat: add recipe entity

---
+++ backend/Foody/src/main/java/com/foody/recipe/entity/Recipe.java
@@ -0,0 +1,29 @@
+package com.foody.recipe.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity(name = "recipe")
+public class Recipe {
+
+    @Id private long id;
+    @Column private String name;
+    @Column private String ingredient;
+    @Column private String description;
+    @Column private String url;
+    @Column private String difficulty;
+    @Column private String servers;
+    @Column private String foodMethod;
+    @Column private String foodSituation;
+    @Column private String foodIngredients;
+    @Column private String foodTypes;
+}

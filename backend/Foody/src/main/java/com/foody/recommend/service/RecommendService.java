28b733752fe4c7a15aeda76bc8f5e5ae85971acb
신상원 <dnjsml30@naver.com>
2023-09-26 22:15:31 +0900

    feat: 재료 기반 추천 dto 생성

--- backend/Foody/src/main/java/com/foody/recommend/service/RecommendService.java
+++ backend/Foody/src/main/java/com/foody/recommend/service/RecommendService.java
@@ -1,9 +1,19 @@
 package com.foody.recommend.service;
 
+import com.foody.recommend.dto.response.RecommendItem;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class RecommendService {
+
+    public List<RecommendItem> findRecommendItemByIngredients(String email) {
+
+        return null;
+    }
 }

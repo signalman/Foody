ccdde00198ca1d3b40ae3380886c421f48b2f1f9
신상원 <dnjsml30@naver.com>
2023-09-27 00:26:36 +0900

    refactor: recipe entity 수정
    Former-commit-id: e9d906f7d355d2598f086f5bbd5108edda9cb757

--- backend/Foody/src/main/java/com/foody/recipe/entity/Recipe.java
+++ backend/Foody/src/main/java/com/foody/recipe/entity/Recipe.java
@@ -1,12 +1,11 @@
 package com.foody.recipe.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @NoArgsConstructor // openCsv때문에 열어놔야함
 @AllArgsConstructor
@@ -20,9 +19,19 @@ public class Recipe {
     @Column(columnDefinition = "TEXT") private String description;
     @Column private String url;
     @Column private String difficulty;
-    @Column private String servers;
+    @Column private double servers;
     @Column private String foodMethod;
     @Column private String foodSituation;
     @Column private String foodIngredients;
     @Column private String foodTypes;
+    @Column private double energy;
+    @Column private double carbohydrates;
+    @Column private double protein;
+    @Column private double dietaryFiber;
+    @Column private double calcium;
+    @Column private double sodium;
+    @Column private double iron;
+    @Column private double fats;
+    @Column private double vitaminA;
+    @Column private double vitaminC;
 }

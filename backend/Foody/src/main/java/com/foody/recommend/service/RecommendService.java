5c97d5db1ccf06592fe17475a81c1adf2760e443
신상원 <dnjsml30@naver.com>
2023-09-26 22:24:42 +0900

    feat: 재료 이어붙이는 기능 구현
    Former-commit-id: 5818fb8bc5312e1bf4a1b9e2cd397936d10fa56e

--- backend/Foody/src/main/java/com/foody/recommend/service/RecommendService.java
+++ backend/Foody/src/main/java/com/foody/recommend/service/RecommendService.java
@@ -1,19 +1,36 @@
 package com.foody.recommend.service;
 
 import com.foody.recommend.dto.response.RecommendItem;
+import com.foody.refrigerators.dto.response.UserRefrigeratorResponse;
+import com.foody.refrigerators.service.RefrigeratorsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class RecommendService {
 
+    private final RefrigeratorsService refrigeratorsService;
+
+    @Transactional
     public List<RecommendItem> findRecommendItemByIngredients(String email) {
 
+        List<UserRefrigeratorResponse> refrigerator = refrigeratorsService.getUserRefrigerator(email);
+        String ingredients = getIngredientsString(refrigerator);
+
         return null;
+    }
+
+    private String getIngredientsString(List<UserRefrigeratorResponse> refrigerator) {
+
+        return refrigerator.stream()
+                .map(UserRefrigeratorResponse::ingredientName)
+                .collect(Collectors.joining(" "));
     }
 }

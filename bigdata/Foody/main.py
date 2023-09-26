bc47974a49bb82a67c1421a3c81313e85e87fee1
신상원 <dnjsml30@naver.com>
2023-09-26 13:37:25 +0900

    feat: recipe 분석기능 구현
    Former-commit-id: b6e116a535ba96826ad7f7b2ab577aefb30f40f0

--- bigdata/Foody/main.py
+++ bigdata/Foody/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from algorithm.router import recipes_router
 
 app = FastAPI()
+
+app.include_router(recipes_router.router, prefix="/recipes", tags=["Recipes"])
 
 
 @app.get("/")

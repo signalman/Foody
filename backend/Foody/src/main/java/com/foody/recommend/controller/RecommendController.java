ec933396629de3b5331a606f9b374eba88a50861
신상원 <dnjsml30@naver.com>
2023-09-26 22:15:31 +0900

    feat: 재료 기반 추천 dto 생성
    Former-commit-id: 03316eb66206207c3d61a2c97d3115b3e2f08193

--- backend/Foody/src/main/java/com/foody/recommend/controller/RecommendController.java
+++ backend/Foody/src/main/java/com/foody/recommend/controller/RecommendController.java
@@ -1,5 +1,6 @@
 package com.foody.recommend.controller;
 
+import com.foody.recommend.dto.response.RecommendItem;
 import com.foody.recommend.service.RecommendService;
 import com.foody.security.util.LoginInfo;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/recommend")
@@ -19,10 +22,12 @@ public class RecommendController {
     private final RecommendService recommendService;
 
     @GetMapping("/ingredients")
-    public ResponseEntity<Void> recommendByIngredients(@AuthenticationPrincipal LoginInfo loginInfo) {
+    public ResponseEntity<List<RecommendItem>> recommendByIngredients(@AuthenticationPrincipal LoginInfo loginInfo) {
 
+        log.debug("{} request recommend By Ingredients", loginInfo.email());
+        List<RecommendItem> recommendItemList = recommendService.findRecommendItemByIngredients(loginInfo.email());
 
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok().body(recommendItemList);
     }
 
 }

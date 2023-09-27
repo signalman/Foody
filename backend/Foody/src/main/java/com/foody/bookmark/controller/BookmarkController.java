117842509f0a91ee68538ebd655f1f1a448c4dc4
신상원 <dnjsml30@naver.com>
2023-09-27 14:03:19 +0900

    feat: 북마크 기능 구현
    Former-commit-id: 06ed2d768905e101f371e441381ff697436dcad1

--- backend/Foody/src/main/java/com/foody/bookmark/controller/BookmarkController.java
+++ backend/Foody/src/main/java/com/foody/bookmark/controller/BookmarkController.java
@@ -1,8 +1,14 @@
 package com.foody.bookmark.controller;
 
+import com.foody.bookmark.service.BookmarkFacade;
 import com.foody.bookmark.service.BookmarkService;
+import com.foody.security.util.LoginInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
+    private final BookmarkFacade bookmarkFacade;
+
+    // 북마크가 있다면 제거, 없다면 추가
+    @GetMapping("/{recipeId}")
+    public ResponseEntity<Void> bookmarkStatusChange(@PathVariable Long recipeId, @AuthenticationPrincipal LoginInfo loginInfo) {
+
+        log.debug(" {} request changed bookmark status", loginInfo.email());
+        bookmarkFacade.changeStatus(recipeId, loginInfo.email());
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- backend/Foody/src/main/java/com/foody/bookmark/repository/BookmarkRepository.java
+++ backend/Foody/src/main/java/com/foody/bookmark/repository/BookmarkRepository.java
@@ -1,12 +1,13 @@
 package com.foody.bookmark.repository;
 
-import com.foody.member.entity.Member;
 import com.foody.bookmark.entity.Bookmark;
-import com.foody.recipe.entity.Recipe;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
-    boolean existsBookmarkByMemberAndRecipe(Member member, Recipe recipe);
+    boolean existsBookmarkByMemberIdAndRecipeId(Long memberId, Long recipeId);
+
+    Optional<Bookmark> findByMemberIdAndRecipeId(Long memberId, Long recipeId);
 
 }

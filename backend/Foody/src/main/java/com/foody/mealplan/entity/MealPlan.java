ace3eb2c42d4ce0c6ec4c4621e519436985e4ff1
apicad0307 <apicad0307@naver.com>
2023-09-29 05:59:18 +0900

    feat: 식단 날짜별 조회 api

--- backend/Foody/src/main/java/com/foody/mealplan/entity/MealPlan.java
+++ backend/Foody/src/main/java/com/foody/mealplan/entity/MealPlan.java
@@ -2,12 +2,12 @@ package com.foody.mealplan.entity;
 
 import com.foody.global.entity.BaseEntity;
 import com.foody.member.entity.Member;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,12 +23,30 @@ public class MealPlan extends BaseEntity {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @Enumerated(value = EnumType.STRING)
-    private MealType mealType;
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "breakfast_id")
+    private Meal breakfast;
 
-    @ManyToOne
-    @JoinColumn(name = "mealplan_food_id")
-    private MealPlan_Food mealPlanFood;
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "lunch_id")
+    private Meal lunch;
 
-    private LocalDateTime dateTime; //ex) 13:40
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "dinner_id")
+    private Meal dinner;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "snack_id")
+    private Meal snack;
+
+    private LocalDate date; //ex) 2023-11-09
+
+    public MealPlan(Member member, LocalDate localDate){
+        this.member = member;
+        this.breakfast = new Meal();
+        this.lunch = new Meal();
+        this.dinner = new Meal();
+        this.snack = new Meal();
+        this.date = localDate;
+    }
 }

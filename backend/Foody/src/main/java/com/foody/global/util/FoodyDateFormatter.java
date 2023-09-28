ca9df9e35a212d8a5ccacc3e2a7c7df05b231335
apicad0307 <apicad0307@naver.com>
2023-09-29 06:00:22 +0900

    feat: 날짜 파싱 util 추가
    Former-commit-id: 6de2ebca6e8aa5656b58f0951720e8d514e0fd6c

--- backend/Foody/src/main/java/com/foody/global/util/FoodyDateFormatter.java
+++ backend/Foody/src/main/java/com/foody/global/util/FoodyDateFormatter.java
@@ -1,7 +1,13 @@
 package com.foody.global.util;
 
-import org.joda.time.format.DateTimeFormatter;
 
-public class DateFormatter extends DateTimeFormatter {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+public class FoodyDateFormatter {
+    public static LocalDate toLocalDate(String dateString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate localDate = LocalDate.parse(dateString.trim(), formatter);
+        return localDate;
+    }
 }

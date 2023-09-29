925ded6f94748e14a8ead3d922c1e2ea615aecae
신상원 <dnjsml30@naver.com>
2023-09-29 15:23:20 +0900

    test: 북마크 조회 테스트

--- backend/Foody/src/test/java/com/foody/util/ControllerTest.java
+++ backend/Foody/src/test/java/com/foody/util/ControllerTest.java
@@ -1,6 +1,12 @@
 package com.foody.util;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.foody.bookmark.controller.BookmarkController;
+import com.foody.bookmark.repository.BookmarkRepository;
+import com.foody.bookmark.service.BookmarkFacade;
+import com.foody.bookmark.service.BookmarkService;
 import com.foody.global.service.AmazonS3Service;
 import com.foody.mbti.controller.MbtiController;
 import com.foody.mbti.repository.MbtiRepository;
@@ -8,6 +14,8 @@ import com.foody.mbti.service.MbtiService;
 import com.foody.member.controller.MemberController;
 import com.foody.member.repository.MemberRepository;
 import com.foody.member.service.MemberService;
+import com.foody.recipe.repository.RecipeRepository;
+import com.foody.recipe.service.RecipeService;
 import com.foody.refrigerators.controller.RefrigeratorsController;
 import com.foody.refrigerators.repository.IngredientRepository;
 import com.foody.refrigerators.service.RefrigeratorsService;
@@ -19,7 +27,8 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
-import javax.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -27,19 +36,34 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @AutoConfigureRestDocs
 @ActiveProfiles("test")
 @MockBean(JpaMetamodelMappingContext.class)
 @AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(RestDocumentationExtension.class)
 @WebMvcTest({
     MemberController.class,
     RefrigeratorsController.class,
-    MbtiController.class
+    MbtiController.class,
+    BookmarkController.class
 })
 public class ControllerTest {
+
+    //setup
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private RestDocumentationContextProvider restDocumentation;
+
     // Util
     @Autowired
     protected MockMvc mockMvc;
@@ -56,12 +80,24 @@ public class ControllerTest {
     @MockBean protected MemberService memberService;
     @MockBean protected RefrigeratorsService refrigeratorsService;
     @MockBean protected MbtiService mbtiService;
+    @MockBean protected RecipeService recipeService;
+    @MockBean protected BookmarkService bookmarkService;
+    @MockBean protected BookmarkFacade bookmarkFacade;
 
     // Repository
     @MockBean protected MemberRepository memberRepository;
     @MockBean protected IngredientRepository ingredientRepository;
     @MockBean protected MbtiRepository mbtiRepository;
+    @MockBean protected RecipeRepository recipeRepository;
+    @MockBean protected BookmarkRepository bookmarkRepository;
 
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                                      .apply(documentationConfiguration(restDocumentation))
+                                      .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                                      .build();
+    }
     protected String createToken(String email, String secretKey) {
 
         final long accessTokenExpireTimeMs = 3600000L; // 1시간

0dfba482c22b619c6a355785420de8def53e39f1
신상원 <dnjsml30@naver.com>
2023-10-01 00:18:44 +0900

    feat: 상태 변화를 위한 update method 생성

--- backend/Foody/src/main/java/com/foody/mbti/entity/Mbti.java
+++ backend/Foody/src/main/java/com/foody/mbti/entity/Mbti.java
@@ -39,4 +39,84 @@ public class Mbti extends BaseEntity {
     private int rawCook; // 날것
     private int etcCook; // 기타
 
+    public void updateKoreanMainDish(int value) {
+        this.koreanMainDish += value;
+    }
+
+    public void updateWesternDish(int value) {
+        this.westernMainDish += value;
+    }
+
+    public void updateSideDish(int value) {
+        this.sideDish += value;
+    }
+
+    public void updateDessert(int value) {
+        this.dessert += value;
+    }
+
+    // 상황별
+    public void updateDailyFood(int value) {
+        this.dailyFood += value;
+    }
+
+    public void updateFestivalFood(int value) {
+        this.festivalFood += value;
+    }
+
+    public void updateConvenienceFood(int value) {
+        this.convenienceFood += value;
+    }
+
+    public void updateSnackFood(int value) {
+        this.snackFood += value;
+    }
+
+    public void updateEtcFood(int value) {
+        this.etcFood += value;
+    }
+
+    // 재료별
+    public void updateMeat(int value) {
+        this.meat += value;
+    }
+
+    public void updateVegetableSeafood(int value) {
+        this.vegetableSeafood += value;
+    }
+
+    public void updateProcessedFood(int value) {
+        this.processedFood += value;
+    }
+
+    public void updateHealthFood(int value) {
+        this.healthFood += value;
+    }
+
+    public void updateGrain(int value) {
+        this.grain += value;
+    }
+
+    // 방법별
+    public void updateLowCook(int value) {
+        this.lowCook += value;
+    }
+
+    public void updateHighCook(int value) {
+        this.highCook += value;
+    }
+
+    public void updateWaterCook(int value) {
+        this.waterCook += value;
+    }
+
+    public void updateRawCook(int value) {
+        this.rawCook += value;
+    }
+
+    public void updateEtcCook(int value) {
+        this.etcCook += value;
+    }
+
+
 }

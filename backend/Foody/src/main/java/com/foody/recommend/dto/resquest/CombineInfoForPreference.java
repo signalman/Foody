cba8392692c87a96469fca223f525013fd9eadd7
신상원 <dnjsml30@naver.com>
2023-09-30 22:11:22 +0900

    feat: 냉장고재료 + 영양소 기반 추천 구현
    Former-commit-id: 4a050679a4587d417bb21a324b4ad29fc32671f6

--- backend/Foody/src/main/java/com/foody/recommend/dto/resquest/CombineInfoForPreference.java
+++ backend/Foody/src/main/java/com/foody/recommend/dto/resquest/CombineInfoForPreference.java
@@ -3,7 +3,7 @@ package com.foody.recommend.dto.resquest;
 import com.foody.mbti.dto.response.MbtiResponse;
 import com.foody.nutrient.dto.response.NutrientResponse;
 
-public record CombineMemberInformation(
+public record CombineInfoForPreference(
     NutrientResponse user_deficiency,
     MbtiResponse user_preference
 ) {

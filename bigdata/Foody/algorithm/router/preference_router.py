3fd9fc326676c2bda56e76ae80933f87598a6787
신상원 <dnjsml30@naver.com>
2023-09-30 17:28:58 +0900

    feat: 추천 시스템 초안 완성

--- bigdata/Foody/algorithm/router/preference_router.py
+++ bigdata/Foody/algorithm/router/preference_router.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
-from pydantic import BaseModel, constr
+from pydantic import BaseModel
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-import random
 from sklearn.preprocessing import MinMaxScaler
-
+from algorithm.database.database import database
+from algorithm.database.model import member, mbti
+from sqlalchemy import select
+import random
 
 router = APIRouter()
 
@@ -85,7 +87,7 @@ encoded_columns = [
 ]
 
 
-# 취향 기반 추천
+# 취향 기반 추천(콘텐츠 기반)
 @router.post("/")
 def get_recommendations_based_on_preference(data: UserPreference, top_k: int = 5):
     # 사용자의 선호도 정보를 배열로 변환
@@ -124,7 +126,7 @@ def get_recommendations_based_on_preference(data: UserPreference, top_k: int = 5
     return top_recipes
 
 
-# 정규화를 사용한 취향 기반 추천
+# 정규화를 사용한 취향 기반 추천(콘텐츠 기반)
 @router.post("/hello")
 def get_normalized_recommendations_based_on_preference(data: UserPreference, top_k: int = 5):
     # 사용자의 선호도 정보를 배열로 변환
@@ -153,6 +155,7 @@ def get_normalized_recommendations_based_on_preference(data: UserPreference, top
     ]
 
     return top_recipes
+
 
 # 취향 + 영양소 기반 추천
 @router.post("/nutrient")
@@ -186,3 +189,159 @@ def get_combined_recommendations_without_ingredients(data: CombineUserInput, top
                     "final_score": recipe_data.iloc[index]['final_score']} for index in final_top_indices]
 
     return {"Top Combined Recommendations Without Ingredients": top_recipes}
+
+
+# 협업 필터링 구현부
+
+@router.post("/collaborative")
+async def collaborative_filtering_recommendation(user_data: UserPreference, top_k: int = 5):
+    # DB에서 모든 사용자의 mbti 정보 가져오기
+    query = select([
+        member.c.id,
+        member.c.weight,
+        member.c.gender,
+        member.c.age,
+        member.c.activity_level,
+        member.c.mbti_id,
+        mbti.c.korean_main_dish,
+        mbti.c.western_main_dish,
+        mbti.c.side_dish,
+        mbti.c.dessert,
+        mbti.c.daily_food,
+        mbti.c.festival_food,
+        mbti.c.convenience_food,
+        mbti.c.snack_food,
+        mbti.c.etc_food,
+        mbti.c.meat,
+        mbti.c.vegetable_seafood,
+        mbti.c.processed_food,
+        mbti.c.health_food,
+        mbti.c.grain,
+        mbti.c.low_cook,
+        mbti.c.high_cook,
+        mbti.c.water_cook,
+        mbti.c.raw_cook,
+        mbti.c.etc_cook
+    ]).select_from(member.join(mbti, member.c.mbti_id == mbti.c.id))
+    all_users_data = await database.fetch_all(query)
+
+    # 사용자-아이템 행렬 구성
+    user_item_matrix = np.array([list(data)[6:] for data in all_users_data])
+
+    # 사용자 간의 코사인 유사도 계산
+    user_similarity = cosine_similarity(user_item_matrix)
+
+    # 해당 사용자와 다른 사용자 간의 유사도를 기반으로 점수 예측
+    user_based_prediction = user_similarity.dot(user_item_matrix) / np.array([np.abs(user_similarity).sum(axis=1)]).T
+
+    # 현재 사용자 데이터 추가
+    current_user_data = np.array([
+        user_data.koreanMainDish,
+        user_data.westernMainDish,
+        user_data.sideDish,
+        user_data.dessert,
+        user_data.dailyFood,
+        user_data.festivalFood,
+        user_data.convenienceFood,
+        user_data.snackFood,
+        user_data.etcFood,
+        user_data.meat,
+        user_data.vegetableSeafood,
+        user_data.processedFood,
+        user_data.healthFood,
+        user_data.grain,
+        user_data.lowCook,
+        user_data.highCook,
+        user_data.waterCook,
+        user_data.rawCook,
+        user_data.etcCook
+    ]).reshape(1, -1)
+
+    # 현재 사용자 데이터를 사용자-아이템 행렬에 추가
+    user_item_matrix = np.vstack([user_item_matrix, current_user_data])
+
+    # 사용자 간의 코사인 유사도 계산
+    user_similarity = cosine_similarity(user_item_matrix)
+
+    # 해당 사용자와 다른 사용자 간의 유사도를 기반으로 점수 예측
+    user_based_prediction = user_similarity.dot(user_item_matrix) / np.array([np.abs(user_similarity).sum(axis=1)]).T
+
+    # 현재 사용자의 예측 점수 계산
+    current_user_prediction = user_based_prediction[-1]  # 마지막 행이 현재 사용자의 예측 점수
+
+    # 상위 top_k개의 아이템 추천
+    top_item_indices = current_user_prediction.argsort()[-top_k:][::-1]
+
+    # 추천된 아이템의 세부 정보 추출
+    top_recipes = [{"recipe_id": int(recipe_data.iloc[index]['recipe_id']),
+                    "predicted_score": current_user_prediction[index]} for index in top_item_indices]
+
+    return {"Top Collaborative Filtering Recommendations": top_recipes}
+
+
+# 협업 필터링 + 아이템 기반 추천 합친 하이브리드
+@router.post("/hybrid")
+async def hybrid_recommendation(user_data: UserPreference, top_k: int = 4):
+    # 각 방식으로부터 추천 받기
+    collaborative_recommendations = await collaborative_filtering_recommendation(user_data, top_k=top_k//2)
+    item_based_recommendations = get_recommendations_based_on_preference(user_data, top_k=top_k//2)
+
+    # 추천 목록 합치기
+    combined_recommendations = collaborative_recommendations[
+                                   "Top Collaborative Filtering Recommendations"] + item_based_recommendations
+
+    # 중복 제거
+    seen_ids = set()
+    unique_recommendations = []
+    for rec in combined_recommendations:
+        if rec['recipe_id'] not in seen_ids:
+            seen_ids.add(rec['recipe_id'])
+            unique_recommendations.append(rec)
+
+    # 중복 제거 후 top_k보다 추천 목록이 적으면 추가로 아이템 추천 받기
+    while len(unique_recommendations) < top_k:
+        additional_recommendation = get_recommendations_based_on_preference(user_data, top_k=1)
+        if additional_recommendation[0]['recipe_id'] not in seen_ids:
+            seen_ids.add(additional_recommendation[0]['recipe_id'])
+            unique_recommendations.append(additional_recommendation[0])
+
+    random.shuffle(unique_recommendations)
+
+    return {"Top Hybrid Recommendations": unique_recommendations}
+
+
+# 조인 테스트
+@router.get("/test")
+async def test():
+    # DB에서 모든 사용자의 mbti 정보 가져오기
+    query = select([
+        member.c.id,
+        member.c.weight,
+        member.c.gender,
+        member.c.age,
+        member.c.activity_level,
+        member.c.mbti_id,
+        mbti.c.korean_main_dish,
+        mbti.c.western_main_dish,
+        mbti.c.side_dish,
+        mbti.c.dessert,
+        mbti.c.daily_food,
+        mbti.c.festival_food,
+        mbti.c.convenience_food,
+        mbti.c.snack_food,
+        mbti.c.etc_food,
+        mbti.c.meat,
+        mbti.c.vegetable_seafood,
+        mbti.c.processed_food,
+        mbti.c.health_food,
+        mbti.c.grain,
+        mbti.c.low_cook,
+        mbti.c.high_cook,
+        mbti.c.water_cook,
+        mbti.c.raw_cook,
+        mbti.c.etc_cook
+    ]).select_from(member.join(mbti, member.c.mbti_id == mbti.c.id))
+
+    all_users_data = await database.fetch_all(query)
+
+    return all_users_data[0]

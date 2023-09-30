3a604505d835d5dec3bdf7489291d892d828d4c5
apicad0307 <apicad0307@naver.com>
2023-10-01 00:37:17 +0900

    feat: 음식 이름 자동완성 검색 기능 구현

--- backend/Foody/src/main/java/com/foody/food/contoller/FoodController.java
+++ backend/Foody/src/main/java/com/foody/food/contoller/FoodController.java
@@ -3,7 +3,6 @@ package com.foody.food.contoller;
 import com.foody.food.service.FoodService;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +16,7 @@ public class FoodController {
 
     private final FoodService foodService;
 
-    @GetMapping
-    public void saveFoodDataToRedis() throws Exception {
-        ClassPathResource resource = new ClassPathResource("food/foody_food.csv");
-        foodService.saveFoodsFromCSV("");
-    }
-
-    @GetMapping("/search-suggest")
+    @GetMapping("/autocomplete")
     public ResponseEntity<Set<String>> getFoodSuggestion(@RequestParam String query){
         Set<String> suggestion = foodService.getFoodSuggestions(query, 10);
         return ResponseEntity.ok()

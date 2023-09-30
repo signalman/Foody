e605fa09b7099749a0f8812446f8b8c6e9b2d257
apicad0307 <apicad0307@naver.com>
2023-09-30 15:31:59 +0900

    fix: 날짜 조회시 빈 DTO로 생성

--- backend/Foody/src/main/java/com/foody/food/repository/FoodSearchJDBCRepository.java
+++ backend/Foody/src/main/java/com/foody/food/repository/FoodSearchJDBCRepository.java
@@ -10,9 +10,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 @Slf4j
 @RequiredArgsConstructor
-public class FoodSearchRepository {
+public class FoodSearchJDBCRepository implements FoodSearchRepository{
 
     private final JdbcTemplate jdbcTemplate;
+
+    @Override
     public void bulkInsert(List<FoodSearch> foodSearchList){
         int batchSize = 1000;
         for (int i = 0; i < foodSearchList.size(); i += batchSize) {
@@ -24,31 +26,32 @@ public class FoodSearchRepository {
             }
         }
     }
-    public boolean isExistsData(){
-        // 128671는 1번째 데이터로, 없으면 insert 진행
-        String sql = "SELECT count(*) FROM FOOD_SEARCH WHERE id = ?";
 
+    @Override
+    public boolean isExistsData(){
+        String sql = "SELECT count(*) FROM FOOD_SEARCH WHERE id = ?";
         int count = jdbcTemplate.queryForObject(sql, new Object[]{8200}, Integer.class);
         return count > 0;
     }
 
     private void batchInsert(List<FoodSearch> foodSearchList) {
-        String sql = "INSERT INTO recipe (id, name, energy, carbohydrates, protein, dietary_fiber, calcium, sodium, iron, fats, vitamin_a, vitamin_c) "
+
+        String sql = "INSERT INTO FOOD_SEARCH (id, name, energy, carbohydrates, protein, dietary_fiber, calcium, sodium, iron, fats, vitamina, vitaminc) "
             + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         jdbcTemplate.batchUpdate(sql, foodSearchList, 1000, (ps, foodSearch) -> {
-            ps.setLong(1, foodSearch.getFoodId());
-            ps.setString(2, foodSearch.getFoodName());
+            ps.setLong(1, foodSearch.getId());
+            ps.setString(2, foodSearch.getName());
             ps.setDouble(3, foodSearch.getEnergy());
             ps.setDouble(4, foodSearch.getCarbohydrates());
             ps.setDouble(5, foodSearch.getProtein());
-            ps.setDouble(6, foodSearch.getDietary_fiber());
+            ps.setDouble(6, foodSearch.getDietaryFiber());
             ps.setDouble(7, foodSearch.getCalcium());
             ps.setDouble(8, foodSearch.getSodium());
             ps.setDouble(9, foodSearch.getIron());
             ps.setDouble(10, foodSearch.getFats());
-            ps.setDouble(11, foodSearch.getVitamin_a());
-            ps.setDouble(12, foodSearch.getVitamin_c());
+            ps.setDouble(11, foodSearch.getVitaminA());
+            ps.setDouble(12, foodSearch.getVitaminC());
         });
     }
 

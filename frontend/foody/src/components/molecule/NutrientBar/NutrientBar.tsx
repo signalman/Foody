bdfd998e3d2af5c95b55e41a5b8dcadd95baf9db
Kim SeoungHyeon <kim0221_@naver.com>
2023-10-02 01:19:52 +0900

    feat: 영양소 막대차트 기능 추가

--- frontend/foody/src/components/molecule/NutrientBar/NutrientBar.tsx
+++ frontend/foody/src/components/molecule/NutrientBar/NutrientBar.tsx
@@ -1,39 +1,56 @@
-import React, { useState } from 'react';
-import { HiOutlineChevronRight } from 'react-icons/hi';
+import React from 'react';
+import { useRecoilState } from 'recoil';
+import nutrientState from 'recoil/atoms/nutrientState';
+import { NutrientTotal } from 'types/meal';
 import NutrientInformation from '../NutrientInformation/NutrientInformation';
 
 interface NutrientBarProps {
-	ismore: boolean;
 	title: string;
-	nutrient: { [key: string]: number };
+	mealNutrient: { [key: string]: number };
+	isempty: boolean;
 }
 
-function NutrientBar({ ismore, title, nutrient }: NutrientBarProps) {
-	const [open, setOpen] = useState<boolean>(false);
+function NutrientBar({ title, mealNutrient, isempty }: NutrientBarProps) {
+	const [totalNutrient] = useRecoilState(nutrientState);
 
-	const openMore = () => {
-		setOpen(true);
-		console.log(open);
+	// const convert: { [Key: string]: keyof NutrientTotal } = {
+	// 	탄수화물: totalNutrient.carbohydrates,
+	// 	단백질: totalNutrient.protein,
+	// 	지방: totalNutrient.fats,
+	// 	식이섬유: totalNutrient.dietaryFiber,
+	// 	칼슘: totalNutrient.calcium,
+	// 	나트륨: totalNutrient.sodium,
+	// 	철분: totalNutrient.iron,
+	// 	비타민A: totalNutrient.vitaminA,
+	// 	비타민C: totalNutrient.vitaminC,
+	// };
+
+	const convert: { [key: string]: keyof NutrientTotal } = {
+		탄수화물: 'carbohydrates',
+		단백질: 'protein',
+		지방: 'fats',
+		식이섬유: 'dietaryFiber',
+		칼슘: 'calcium',
+		나트륨: 'sodium',
+		철분: 'iron',
+		비타민A: 'vitaminA',
+		비타민C: 'vitaminC',
 	};
 
 	return (
 		<div>
-			<div className="nutirent-of-day-title-box">
-				<p className="nutrient-of-day-title">{title}</p>
-				{ismore && (
-					<button className="nutrient-of-day-button" onClick={openMore} type="button">
-						<p>더보기</p>
-						<HiOutlineChevronRight size={14} />
-					</button>
-				)}
-			</div>
+			{isempty && (
+				<div className="nutirent-of-day-title-box">
+					<p className="nutrient-of-day-title">{title}</p>
+				</div>
+			)}
 			<div className="nutrient-main-box">
-				{Object.keys(nutrient).map((nutrientName) => (
+				{Object.keys(mealNutrient).map((nutrientName) => (
 					<NutrientInformation
 						key={nutrientName}
 						nutrient={nutrientName}
-						totalNutrient={300} // 여기에 영양소에 대한 적절한 기본값 또는 측정된 값 사용
-						valueNutrient={nutrient[nutrientName]} // 객체의 키를 사용하여 해당 영양소의 값을 가져옵니다.
+						totalNutrient={totalNutrient[convert[nutrientName]]} // 여기에 영양소에 대한 적절한 기본값 또는 측정된 값 사용
+						valueNutrient={mealNutrient[nutrientName]} // 객체의 키를 사용하여 해당 영양소의 값을 가져옵니다.
 					/>
 				))}
 			</div>

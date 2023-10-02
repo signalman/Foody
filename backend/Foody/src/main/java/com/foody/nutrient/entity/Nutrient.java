5356518392e3a2864f742e039b57adf158857ceb
이규민 <lkm454545@naver.com>
2023-10-02 16:37:09 +0900

    fix: 추천용 영양정보 조회 메서드 수정
    Former-commit-id: 183e11145565402b8f42364b2059435cdbc095e9

--- backend/Foody/src/main/java/com/foody/nutrient/entity/Nutrient.java
+++ backend/Foody/src/main/java/com/foody/nutrient/entity/Nutrient.java
@@ -38,4 +38,8 @@ public class Nutrient extends BaseEntity {
             nutrientResponse.sodium(), nutrientResponse.iron(), nutrientResponse.fats(),
             nutrientResponse.vitaminA(), nutrientResponse.vitaminC());
     }
+
+    public static Nutrient makeNutrient(double[] arr) {
+        return new Nutrient(arr[0],arr[1],arr[2],arr[3],arr[4],arr[5],arr[6],arr[7],arr[8],arr[9]);
+    }
 }

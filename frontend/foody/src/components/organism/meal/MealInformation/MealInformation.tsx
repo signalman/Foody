6611e2d75ba32e692c94a844c0f4f45f75879dec
Kim SeoungHyeon <kim0221_@naver.com>
2023-10-03 17:06:22 +0900

    feat: 식단 홈페이지 관련 컴포넌트 data값 받을 수 있게 기능 추가

--- frontend/foody/src/components/organism/meal/MealInformation/MealInformation.tsx
+++ frontend/foody/src/components/organism/meal/MealInformation/MealInformation.tsx
@@ -1,28 +1,66 @@
-import React from 'react';
+import React, { Dispatch } from 'react';
 import './MealInformation.scss';
 import DayofMealPart from 'components/molecule/DayofMealPart/DayofMealPart';
 import { Meal } from 'types/meal';
+import { breakfastState, dinnerState, lunchState, snackState } from 'recoil/atoms/nutrientState';
+import { useRecoilValue } from 'recoil';
 
 interface DailyMeals {
 	breakfast: Meal;
 	lunch: Meal;
 	dinner: Meal;
 	snack: Meal;
+	setDetailOpen: Dispatch<React.SetStateAction<boolean>>;
+	setSearchOpen: Dispatch<React.SetStateAction<boolean>>;
+	setMeal: Dispatch<React.SetStateAction<string>>;
 }
 
-function MealInformation({ breakfast, lunch, dinner, snack }: DailyMeals) {
-	console.log(breakfast);
-	console.log(lunch);
-	console.log(dinner);
-	console.log(snack);
+function MealInformation({ setDetailOpen, setSearchOpen, setMeal, breakfast, lunch, dinner, snack }: DailyMeals) {
+	const breakState = useRecoilValue(breakfastState);
+	const lunState = useRecoilValue(lunchState);
+	const dinState = useRecoilValue(dinnerState);
+	const snaState = useRecoilValue(snackState);
+
 	return (
 		<div className="meal-information-box">
 			<p className="meal-information-title">일일 식단 정보</p>
 			<div className="meal-information">
-				<DayofMealPart goal={373} meal="아침" value={200} />
-				<DayofMealPart goal={373} meal="점심" value={200} />
-				<DayofMealPart goal={373} meal="저녁" value={200} />
-				<DayofMealPart goal={373} meal="간식" value={200} />
+				<DayofMealPart
+					setMeal={setMeal}
+					setDetailOpen={setDetailOpen}
+					setSearchOpen={setSearchOpen}
+					imgsrc={breakfast.imgUrl}
+					goal={breakState.energy}
+					meal="아침"
+					value={breakfast.total.energy}
+				/>
+				<DayofMealPart
+					setMeal={setMeal}
+					setDetailOpen={setDetailOpen}
+					setSearchOpen={setSearchOpen}
+					imgsrc={lunch.imgUrl}
+					goal={lunState.energy}
+					meal="점심"
+					value={lunch.total.energy}
+				/>
+				<DayofMealPart
+					setMeal={setMeal}
+					setDetailOpen={setDetailOpen}
+					setSearchOpen={setSearchOpen}
+					imgsrc={dinner.imgUrl}
+					goal={dinState.energy}
+					meal="저녁"
+					value={dinner.total.energy}
+				/>
+				<DayofMealPart
+					setMeal={setMeal}
+					setDetailOpen={setDetailOpen}
+					setSearchOpen={setSearchOpen}
+					imgsrc={snack.imgUrl}
+					goal={snaState.energy}
+					meal="간식"
+					value={snack.total.energy}
+				/>
 			</div>
 		</div>
 	);

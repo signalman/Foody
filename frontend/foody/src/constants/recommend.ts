9f25a507f2f6d2649a71ed5fb2f03f04be9fa401
임희선 <gmltjs4107@gmail.com>
2023-10-03 19:46:02 +0900

    refactor: user nickname 받아오기, toast 띄우기, 매니페스트 설정 수정

--- frontend/foody/src/constants/recommend.ts
+++ frontend/foody/src/constants/recommend.ts
@@ -1,5 +1,5 @@
 const RECOMMEND_LIST = [
-	{ index: 'hybrid', title: 'OO님의 입맛에 딱 맞는 음식' },
+	{ index: 'hybrid', title: `OO 님의 입맛에 딱 맞는 음식` },
 	{ index: 'preference', title: '건강을 채우는 음식' },
 	{ index: 'ingredients', title: '직접 만들어 보세요!' },
 ];

4363f5c174c7242a92f44d1be1e8fb7d697d8f6b
Kim SeoungHyeon <kim0221_@naver.com>
2023-10-04 20:11:09 +0900

    feat: 직접식단 입력  컴포넌트 기능 추가

--- frontend/foody/src/components/organism/meal/RegisterMealSelf/RegisterMealSelf.tsx
+++ frontend/foody/src/components/organism/meal/RegisterMealSelf/RegisterMealSelf.tsx
@@ -3,8 +3,18 @@ import './RegisterMealSelf.scss';
 import React, { useEffect, useState } from 'react';
 import LargeButton from 'components/atom/LargeButton/LargeButton';
 import LargeButtonColor from 'constants/color';
+import Layout from 'components/template/Layout/Layout';
+import LayoutBottomMargin, { LayoutTopMargin } from 'constants/Margin';
+import LayoutPadding from 'constants/Padding';
+import SubHeader from 'components/organism/SubHeader/SubHeader';
+import { RegistMeal } from 'types/meal';
 
-function RegisterMealSelf() {
+interface RegisterMealSelfProps {
+	setWrite: React.Dispatch<React.SetStateAction<boolean>>;
+	setRegistMealk: React.Dispatch<React.SetStateAction<RegistMeal | null>>;
+	setSelfName: React.Dispatch<React.SetStateAction<string>>;
+}
+function RegisterMealSelf({ setWrite, setRegistMealk, setSelfName }: RegisterMealSelfProps) {
 	const [name, setName] = useState<string>('');
 	const [cal, setCal] = useState<string>('');
 	const [tan, setTan] = useState<string>('');
@@ -13,7 +23,7 @@ function RegisterMealSelf() {
 	const [sik, setSik] = useState<string>('');
 	const [sume, setSume] = useState<string>('');
 	const [na, setNa] = useState<string>('');
-	const [iron, setIron] = useState<string>('');
+	const [ir, setIron] = useState<string>('');
 	const [vitaA, setVitaA] = useState<string>('');
 	const [vitaC, setVitaC] = useState<string>('');
 
@@ -50,116 +60,151 @@ function RegisterMealSelf() {
 	}, [name, cal]);
 
 	const registerButton = () => {
-		console.log(1);
+		const energy = parseFloat(cal === '' ? '0' : cal);
+		const carbohydrates = parseFloat(tan === '' ? '0' : tan);
+		const protein = parseFloat(dan === '' ? '0' : dan);
+		const dietaryFiber = parseFloat(sik === '' ? '0' : sik);
+		const calcium = parseFloat(sume === '' ? '0' : sume);
+		const sodium = parseFloat(na === '' ? '0' : na);
+		const iron = parseFloat(ir === '' ? '0' : ir);
+		const fats = parseFloat(ji === '' ? '0' : ji);
+		const vitaminA = parseFloat(vitaA === '' ? '0' : vitaA);
+		const vitaminC = parseFloat(vitaC === '' ? '0' : vitaC);
+
+		setRegistMealk({
+			name,
+			nutrientRequest: {
+				energy,
+				carbohydrates,
+				protein,
+				dietaryFiber,
+				calcium,
+				sodium,
+				iron,
+				fats,
+				vitaminA,
+				vitaminC,
+			},
+		});
+		setSelfName(name);
+		setWrite(false);
 	};
+
+	const handleMove = () => {
+		setWrite(false);
+	};
+
 	return (
 		<div>
-			<MealInput
-				isValid={inputNameCheck}
-				onChangeValue={setName}
-				placeholder="ex. 김치찌개"
-				requireIcon
-				title="음식이름"
-				unit=""
-				value={name}
-			/>
-			<MealInput
-				isValid={inputNutrientCheck}
-				onChangeValue={setCal}
-				placeholder="ex. 370"
-				requireIcon
-				title="1회 섭취 칼로리"
-				unit="kcal"
-				value={cal}
-			/>
-			<p className="register-info">영양정보</p>
-			<MealInput
-				isValid={inputNutrientCheck}
-				onChangeValue={setTan}
-				placeholder="ex. 100"
-				requireIcon={false}
-				title="탄수화물"
-				unit="g"
-				value={tan}
-			/>
-			<MealInput
-				isValid={inputNutrientCheck}
-				onChangeValue={setDan}
-				placeholder="ex. 100"
-				requireIcon={false}
-				title="단백질"
-				unit="g"
-				value={dan}
-			/>
-			<MealInput
-				isValid={inputNutrientCheck}
-				onChangeValue={setJi}
-				placeholder="ex. 100"
-				requireIcon={false}
-				title="지방"
-				unit="g"
-				value={ji}
-			/>
-			<MealInput
-				isValid={inputNutrientCheck}
-				onChangeValue={setSik}
-				placeholder="ex. 100"
-				requireIcon={false}
-				title="식이섬유"
-				unit="g"
-				value={sik}
-			/>
-			<MealInput
-				isValid={inputNutrientCheck}
-				onChangeValue={setNa}
-				placeholder="ex. 100"
-				requireIcon={false}
-				title="나트륨"
-				unit="mg"
-				value={na}
-			/>
-			<MealInput
-				isValid={inputNutrientCheck}
-				onChangeValue={setSume}
-				placeholder="ex. 100"
-				requireIcon={false}
-				title="칼슘"
-				unit="mg"
-				value={sume}
-			/>
-			<MealInput
-				isValid={inputNutrientCheck}
-				onChangeValue={setIron}
-				placeholder="ex. 100"
-				requireIcon={false}
-				title="철분"
-				unit="mg"
-				value={iron}
-			/>
-			<MealInput
-				isValid={inputNutrientCheck}
-				onChangeValue={setVitaA}
-				placeholder="ex. 100"
-				requireIcon={false}
-				title="비타민A"
-				unit="ug"
-				value={vitaA}
-			/>
-			<MealInput
-				isValid={inputNutrientCheck}
-				onChangeValue={setVitaC}
-				placeholder="ex. 100"
-				requireIcon={false}
-				title="비타민C"
-				unit="mg"
-				value={vitaC}
-			/>
-			<LargeButton
-				buttonClick={registerButton}
-				buttonColor={check ? LargeButtonColor.Green : LargeButtonColor.Gray}
-				value="입력 완료하기"
-				imgsrc=""
-			/>
+			<SubHeader title="직접 입력하기" isBack={false} handleMove={handleMove} />
+			<Layout marginBottom={LayoutBottomMargin.mb20} marginTop={LayoutTopMargin.mt10} padding={LayoutPadding.p30}>
+				<MealInput
+					isValid={inputNameCheck}
+					onChangeValue={setName}
+					placeholder="ex. 김치찌개"
+					requireIcon
+					title="음식이름"
+					unit=""
+					value={name}
+				/>
+				<MealInput
+					isValid={inputNutrientCheck}
+					onChangeValue={setCal}
+					placeholder="ex. 370"
+					requireIcon
+					title="1회 섭취 칼로리"
+					unit="kcal"
+					value={cal}
+				/>
+				<p className="register-info">영양정보</p>
+				<MealInput
+					isValid={inputNutrientCheck}
+					onChangeValue={setTan}
+					placeholder="ex. 100"
+					requireIcon={false}
+					title="탄수화물"
+					unit="g"
+					value={tan}
+				/>
+				<MealInput
+					isValid={inputNutrientCheck}
+					onChangeValue={setDan}
+					placeholder="ex. 100"
+					requireIcon={false}
+					title="단백질"
+					unit="g"
+					value={dan}
+				/>
+				<MealInput
+					isValid={inputNutrientCheck}
+					onChangeValue={setJi}
+					placeholder="ex. 100"
+					requireIcon={false}
+					title="지방"
+					unit="g"
+					value={ji}
+				/>
+				<MealInput
+					isValid={inputNutrientCheck}
+					onChangeValue={setSik}
+					placeholder="ex. 100"
+					requireIcon={false}
+					title="식이섬유"
+					unit="g"
+					value={sik}
+				/>
+				<MealInput
+					isValid={inputNutrientCheck}
+					onChangeValue={setNa}
+					placeholder="ex. 100"
+					requireIcon={false}
+					title="나트륨"
+					unit="mg"
+					value={na}
+				/>
+				<MealInput
+					isValid={inputNutrientCheck}
+					onChangeValue={setSume}
+					placeholder="ex. 100"
+					requireIcon={false}
+					title="칼슘"
+					unit="mg"
+					value={sume}
+				/>
+				<MealInput
+					isValid={inputNutrientCheck}
+					onChangeValue={setIron}
+					placeholder="ex. 100"
+					requireIcon={false}
+					title="철분"
+					unit="mg"
+					value={ir}
+				/>
+				<MealInput
+					isValid={inputNutrientCheck}
+					onChangeValue={setVitaA}
+					placeholder="ex. 100"
+					requireIcon={false}
+					title="비타민A"
+					unit="ug"
+					value={vitaA}
+				/>
+				<MealInput
+					isValid={inputNutrientCheck}
+					onChangeValue={setVitaC}
+					placeholder="ex. 100"
+					requireIcon={false}
+					title="비타민C"
+					unit="mg"
+					value={vitaC}
+				/>
+				<LargeButton
+					buttonClick={registerButton}
+					buttonColor={check ? LargeButtonColor.Green : LargeButtonColor.Gray}
+					value="입력 완료하기"
+					imgsrc=""
+				/>
+			</Layout>
 		</div>
 	);
 }

6e29d7cfa7165008dcbefc73f95e92e0742282fe
Kim SeoungHyeon <kim0221_@naver.com>
2023-10-04 09:14:16 +0900

    feat: 상세식단 title 컴포넌트 생성

---
+++ frontend/foody/src/components/atom/DetailMealCheck/DetailMealCheck.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { RiDeleteBinFill } from 'react-icons/ri';
+
+interface DetailMealCheckProps {
+	onDelete: () => void;
+}
+function DetailMealCheck({ onDelete }: DetailMealCheckProps) {
+	return (
+		<div className="detail-meal-check-container">
+			<p>해당 음식이 아닌가요?</p>
+			<button type="button" onClick={onDelete} className="detail-meal-check-btn">
+				<RiDeleteBinFill size={10} />
+				<p>삭제하기</p>
+			</button>
+		</div>
+	);
+}
+
+export default DetailMealCheck;

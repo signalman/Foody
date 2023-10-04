0e36d4b9f3faee0f58c5a7a147eaa2c77d4061b9
Kim SeoungHyeon <kim0221_@naver.com>
2023-10-04 20:10:42 +0900

    feat: DetialMealTitle 컴포넌트 수정

--- frontend/foody/src/components/atom/DetailMealTitle/DetailMealTitle.tsx
+++ frontend/foody/src/components/atom/DetailMealTitle/DetailMealTitle.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HiOutlineChevronRight } from 'react-icons/hi';
+import './DetailMealTitle.scss';
+import { NutrientTotal } from 'types/meal';
+import MealDetailNutrient from 'components/organism/meal/MealDetailNutrient/MealDetailNutrient';
 
 interface DetailMealTitleProps {
 	foodName: string;
 	foodEnergy: number;
-	openFoodInfo: () => void;
+	nutrient: NutrientTotal;
 }
-function DetailMealTitle({ foodName, foodEnergy, openFoodInfo }: DetailMealTitleProps) {
+function DetailMealTitle({ foodName, foodEnergy, nutrient }: DetailMealTitleProps) {
+	const [open, setOpen] = useState<boolean>(false);
+
+	const openMealInfo = () => {
+		setOpen(true);
+	};
+
 	return (
 		<div className="detail-meal-title-container">
 			<p>{foodName}</p>
-			<button type="button" onClick={openFoodInfo} className="detail-meal-title-btn">
-				<p>{foodEnergy}</p>
-				<HiOutlineChevronRight size={16} />
+			<button type="button" onClick={openMealInfo} className="detail-meal-title-btn">
+				<p>{foodEnergy}kcal</p>
+				<HiOutlineChevronRight size={10} />
 			</button>
+			{open && <MealDetailNutrient name={foodName} nutrient={nutrient} open={open} setOpen={setOpen} />}
 		</div>
 	);
 }

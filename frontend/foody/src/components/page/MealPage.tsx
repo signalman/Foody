332161faed687a814e4639adc0042adeef0aef19
Kim SeoungHyeon <kim0221_@naver.com>
2023-10-04 22:54:12 +0900

    feat: 식단 페이지 렌더링 처리

--- frontend/foody/src/components/page/MealPage.tsx
+++ frontend/foody/src/components/page/MealPage.tsx
@@ -48,13 +48,20 @@ function MealPage() {
 	const dinState = useRecoilValue(dinnerState);
 	const snaState = useRecoilValue(snackState);
 
+	const [deleteOk, setDeleteOk] = useState<boolean>(false);
+
+	const [nowmonth, setMonth] = useState<string>('');
+	const [nowday, setDay] = useState<string>('');
+
 	useEffect(() => {
 		const dateObject = new Date(selectedDate);
-
+		// year, month, day 자르기
 		const year = dateObject.getFullYear();
 		const month = String(dateObject.getMonth() + 1).padStart(2, '0');
 		const day = String(dateObject.getDate()).padStart(2, '0');
-
+		setMonth(month);
+		setDay(day);
+		// sendDate 처리
 		const sendDate = `${year}-${month}-${day}`;
 		setGetDate(sendDate);
 
@@ -65,11 +72,10 @@ function MealPage() {
 			setSnack(response.data.snack);
 			setTotal(response.data.total);
 		});
-		console.log(selectedDate);
 
 		setDisplayMonth(selectedDate);
-	}, [selectedDate]);
-	console.log(breakfast);
+		setDeleteOk(false);
+	}, [selectedDate, deleteOk, meal, breakfast.foods.length]);
 
 	if (searchOpen === true && meal) {
 		return <MealSearch setOpen={setSearchOpen} meal={meal} selectedDate={getDate} />;
@@ -77,16 +83,60 @@ function MealPage() {
 
 	if (detailOpen === true && meal) {
 		if (meal === '아침') {
-			return <DetailMeal selectedDate={getDate} meal={meal} getData={breakfast} requireData={breakState} />;
+			return (
+				<DetailMeal
+					selectedDate={getDate}
+					meal={meal}
+					getData={breakfast}
+					requireData={breakState}
+					setDeleteOk={setDeleteOk}
+					setDetailOpen={setDetailOpen}
+					month={nowmonth}
+					day={nowday}
+				/>
+			);
 		}
 		if (meal === '점심') {
-			return <DetailMeal selectedDate={getDate} meal={meal} getData={lunch} requireData={lunState} />;
+			return (
+				<DetailMeal
+					selectedDate={getDate}
+					meal={meal}
+					getData={lunch}
+					requireData={lunState}
+					setDeleteOk={setDeleteOk}
+					setDetailOpen={setDetailOpen}
+					month={nowmonth}
+					day={nowday}
+				/>
+			);
 		}
 		if (meal === '저녁') {
-			return <DetailMeal selectedDate={getDate} meal={meal} getData={dinner} requireData={dinState} />;
+			return (
+				<DetailMeal
+					selectedDate={getDate}
+					meal={meal}
+					getData={dinner}
+					requireData={dinState}
+					setDeleteOk={setDeleteOk}
+					setDetailOpen={setDetailOpen}
+					month={nowmonth}
+					day={nowday}
+				/>
+			);
 		}
 		if (meal === '간식') {
-			return <DetailMeal selectedDate={getDate} meal={meal} getData={snack} requireData={snaState} />;
+			return (
+				<DetailMeal
+					selectedDate={getDate}
+					meal={meal}
+					getData={snack}
+					requireData={snaState}
+					setDeleteOk={setDeleteOk}
+					setDetailOpen={setDetailOpen}
+					month={nowmonth}
+					day={nowday}
+				/>
+			);
 		}
 	}
 

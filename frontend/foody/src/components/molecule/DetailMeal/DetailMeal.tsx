391909317e2bef2260c1fe2beeb015d3cbe81899
Kim SeoungHyeon <kim0221_@naver.com>
2023-10-04 09:20:48 +0900

    feat: 상세식단 컴포넌트 생성

--- frontend/foody/src/components/molecule/DetailMeal/DetailMeal.tsx
+++ frontend/foody/src/components/molecule/DetailMeal/DetailMeal.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { GetMealData, NutrientTotal } from 'types/meal';
+import CheckMeal from '../CheckMeal/CheckMeal';
+import DetailMealTable from '../DetailMealTable/DetailMealTable';
 
-function DetailMeal() {
-	return <div>hello</div>;
+interface DetailMealProps {
+	selectedDate: string;
+	meal: string;
+	getData: GetMealData;
+	requireData: NutrientTotal;
+}
+function DetailMeal({ selectedDate, meal, getData, requireData }: DetailMealProps) {
+	return (
+		<div>
+			<p>{selectedDate}</p>
+			<CheckMeal
+				dan={getData.total.protein}
+				ji={getData.total.fats}
+				meal={meal}
+				na={getData.total.sodium}
+				tan={getData.total.carbohydrates}
+				mealTotal={requireData.energy}
+				mealValue={getData.total.energy}
+			/>
+			<p>hello</p>
+			<DetailMealTable foods={getData.foods} />
+		</div>
+	);
 }
 
 export default DetailMeal;

54eb0c591db93a2832034401deaa2687b3b7f38b
임희선 <gmltjs4107@gmail.com>
2023-10-05 09:18:28 +0900

    feat: 마이메뉴 구현

--- frontend/foody/src/components/organism/Header/Header.tsx
+++ frontend/foody/src/components/organism/Header/Header.tsx
@@ -5,12 +5,14 @@ import { AiOutlineMenu } from 'react-icons/ai';
 import { GoBell } from 'react-icons/go';
 import { Link } from 'react-router-dom';
 import toast from 'react-hot-toast';
+import MyMenu from '../MyMenu/MyMenu';
 
 function Header() {
 	const [newNoti, setNewNoti] = useState(false);
+	const [isOpenMyMenu, setIsOpenMyMenu] = useState(false);
 
 	const handleMenu = () => {
-		alert('마이페이지');
+		setIsOpenMyMenu(true);
 	};
 
 	// TODOS: 알림이 있으면 새로운 알림이 있다고 알려주기
@@ -22,17 +24,20 @@ function Header() {
 	};
 
 	return (
-		<header className="header-container">
-			<button type="button" onClick={handleMenu}>
-				<AiOutlineMenu size={24} />
-			</button>
-			<Link to="/" className="header-logo-container">
-				<img src={logoImg} alt="logo" />
-			</Link>
-			<button type="button" className={newNoti ? 'bell' : ''} onClick={handleNotice}>
-				<GoBell size={25} />
-			</button>
-		</header>
+		<>
+			<header className="header-container">
+				<button type="button" onClick={handleMenu}>
+					<AiOutlineMenu size={24} />
+				</button>
+				<Link to="/" className="header-logo-container">
+					<img src={logoImg} alt="logo" />
+				</Link>
+				<button type="button" className={newNoti ? 'bell' : ''} onClick={handleNotice}>
+					<GoBell size={25} />
+				</button>
+			</header>
+			<MyMenu isOpenMyMenu={isOpenMyMenu} setIsOpenMyMenu={setIsOpenMyMenu} />
+		</>
 	);
 }
 

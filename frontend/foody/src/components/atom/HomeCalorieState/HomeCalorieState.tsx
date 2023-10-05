04c6d61f94566121b3f5822940d5eb198e1919fa
Kim SeoungHyeon <kim0221_@naver.com>
2023-10-06 02:21:39 +0900

    fix: 메인 페이지 렌더링 수정

--- frontend/foody/src/components/atom/HomeCalorieState/HomeCalorieState.tsx
+++ frontend/foody/src/components/atom/HomeCalorieState/HomeCalorieState.tsx
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './HomeCalorieState.scss';
 import { Doughnut } from 'react-chartjs-2';
 import { useRecoilValue } from 'recoil';
-import nutrientState from 'recoil/atoms/nutrientState';
+import nutrientState, { isChangeMealNutrientState } from 'recoil/atoms/nutrientState';
 import getDayofMeal from 'utils/api/meal';
+import { DonutChartDataType } from 'types/receipt';
+
+const defaultData = {
+	labels: ['채워진 열량', '남은 권장 열량'],
+	datasets: [
+		{
+			data: [0, 0],
+			backgroundColor: ['#1aa274', '#eee'],
+			borderColor: ['#fff'],
+			borderWidth: 1,
+			circumference: 360,
+			rotation: 0,
+			cutout: '85%',
+		},
+	],
+};
 
 const options = {
 	plugins: {
@@ -16,25 +32,11 @@ const options = {
 function HomeCalorieState() {
 	const nutrient = useRecoilValue(nutrientState);
 	const [todayCurrentEnergy, setTodayCurrentEnergy] = useState<number | null>(null);
-	const data = {
-		labels: ['채워진 열량', '남은 권장 열량'],
-		datasets: [
-			{
-				data: [
-					todayCurrentEnergy,
-					nutrient.energy - (todayCurrentEnergy || 0) < 0 ? 0 : nutrient.energy - (todayCurrentEnergy || 0),
-				],
-				backgroundColor: ['#1aa274', '#eee'],
-				borderColor: ['#fff'],
-				borderWidth: 1,
-				circumference: 360,
-				rotation: 0,
-				cutout: '85%',
-			},
-		],
-	};
+	const [data, setData] = useState<DonutChartDataType | null>(null);
+	const isChangeMealNutrient = useRecoilValue(isChangeMealNutrientState);
 
-	useState(() => {
+	useEffect(() => {
+		console.log('실행');
 		if (!todayCurrentEnergy) {
 			const dateObject = new Date();
 
@@ -48,15 +50,60 @@ function HomeCalorieState() {
 				if (currentData >= nutrient) {
 					currentData = nutrient;
 				}
+				setData({
+					labels: ['채워진 열량', '남은 권장 열량'],
+					datasets: [
+						{
+							data: [currentData, nutrient.energy - (currentData || 0) < 0 ? 0 : nutrient.energy - (currentData || 0)],
+							backgroundColor: ['#1aa274', '#eee'],
+							borderColor: ['#fff'],
+							borderWidth: 1,
+							circumference: 360,
+							rotation: 0,
+							cutout: '85%',
+						},
+					],
+				});
 				setTodayCurrentEnergy(currentData);
 			});
 		}
-	});
+	}, [nutrient, todayCurrentEnergy]);
+
+	useEffect(() => {
+		const dateObject = new Date();
+
+		const year = dateObject.getFullYear();
+		const month = String(dateObject.getMonth() + 1).padStart(2, '0');
+		const day = String(dateObject.getDate()).padStart(2, '0');
+
+		const sendDate = `${year}-${month}-${day}`;
+		getDayofMeal(sendDate).then((response) => {
+			let currentData = response.data.total.energy;
+			if (currentData >= nutrient) {
+				currentData = nutrient;
+			}
+			setData({
+				labels: ['채워진 열량', '남은 권장 열량'],
+				datasets: [
+					{
+						data: [currentData, nutrient.energy - (currentData || 0) < 0 ? 0 : nutrient.energy - (currentData || 0)],
+						backgroundColor: ['#1aa274', '#eee'],
+						borderColor: ['#fff'],
+						borderWidth: 1,
+						circumference: 360,
+						rotation: 0,
+						cutout: '85%',
+					},
+				],
+			});
+			setTodayCurrentEnergy(currentData);
+		});
+	}, [isChangeMealNutrient, nutrient]);
 
 	return (
 		<div className="home-calorie-state-container">
 			<div className="doughnut-wrap">
-				<Doughnut data={data} options={options} />
+				<Doughnut data={data || defaultData} options={options} />
 				<div className="doughnut-percent">
 					{parseFloat((((todayCurrentEnergy || 1) / nutrient.energy) * 100).toFixed(0))}%
 				</div>

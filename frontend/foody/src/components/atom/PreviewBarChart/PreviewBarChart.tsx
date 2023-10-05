f3b35b67dcd48060eb25bd6825674fed9ebffb4c
임희선 <gmltjs4107@gmail.com>
2023-10-05 17:00:56 +0900

    refactor: 열량 미리보기 수정

--- frontend/foody/src/components/atom/PreviewBarChart/PreviewBarChart.tsx
+++ frontend/foody/src/components/atom/PreviewBarChart/PreviewBarChart.tsx
@@ -1,34 +1,33 @@
 import React from 'react';
-import './BarChart.scss';
+import './PreviewBarChart.scss';
 import classNames from 'classnames';
 import { BarColor, PreviewBarColor } from 'constants/color';
 
-interface BarChartProps {
+interface PreviewBarChartProps {
 	total: number;
 	value: number;
 	previewValue?: number;
 	barcolor: BarColor;
 }
 
-function BarChart({ total, value, previewValue = 0, barcolor }: BarChartProps) {
+function PreviewBarChart({ total, value, previewValue = 0, barcolor }: PreviewBarChartProps) {
 	const totalWidth = 100;
 
 	let barWidth: number = (value / total) * totalWidth; // n%
-	const checkBar: number = barWidth / totalWidth; // 0.0n
 	let previewBarWidth: number = (previewValue / total) * totalWidth;
+	// const checkBarWidth: number = ; // n%
+	// const checkBar: number = (value + previewValue) / totalWidth; // 0.0n
+	// let previewBarWidth: number = (previewValue / total) * totalWidth;
 
 	let checkColor = barcolor; // 기본 색상은 barcolor 그대로 사용
 	let previewColor: PreviewBarColor = PreviewBarColor.Null;
 
-	if (barcolor === 'noColor') {
-		// barcolor가 'null'인 경우
-		if (checkBar <= 0.33 || checkBar >= 1.01) {
-			checkColor = BarColor.Red; // 빨간색
-			previewColor = PreviewBarColor.Red;
-		} else if (checkBar >= 0.33 && checkBar <= 1.01) {
-			checkColor = BarColor.Green; // 초록색
-			previewColor = PreviewBarColor.Green;
-		}
+	if (previewBarWidth >= 34 && previewBarWidth <= 105) {
+		checkColor = BarColor.Green; // 초록색
+		previewColor = PreviewBarColor.Green;
+	} else {
+		checkColor = BarColor.Red; // 빨간색
+		previewColor = PreviewBarColor.Red;
 	}
 
 	if (barWidth >= totalWidth) {
@@ -40,14 +39,14 @@ function BarChart({ total, value, previewValue = 0, barcolor }: BarChartProps) {
 
 	return (
 		<div className="bar-box">
-			<div className={classNames('bar-content', checkColor)} style={{ width: `calc(${barWidth}% - 10px)` }} />
+			<div className={classNames('bar-content', checkColor)} style={{ width: `${barWidth}%` }} />
 			<div className={classNames('preview-bar', previewColor)} style={{ width: `${previewBarWidth}%` }} />
 		</div>
 	);
 }
 
-export default BarChart;
+export default PreviewBarChart;
 
-BarChart.defaultProps = {
+PreviewBarChart.defaultProps = {
 	previewValue: 0,
 };
